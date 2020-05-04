00c36bf08834a1b9e2ce6aeb411ee84dcb41b29b
GTFB <20010810@academ-x.ru>
2020-05-04 23:22:22 +0300

    Settings Rout
    settings routs with icons

---
+++ resources/modules/admin/src/components/Assets.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+
+export default class Assets extends Component{
+  render(){
+    return <div>Assets</div>;
+  }
+
+}
---
+++ resources/modules/admin/src/components/Templates.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+
+export default class Templates extends Component{
+  render(){
+    return <div>Templates</div>;
+  }
+
+}
