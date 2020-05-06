8516eb01dc5268bd207dc74b8615e26873542546
shtempel <o_gredi@mail.ru>
2020-05-06 16:56:49 +0300

    new templates controller

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -2,80 +2,132 @@
 
 namespace App\Http\Controllers;
 
+use App\Constructor\Template;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
-use App\Template;
+use Illuminate\Support\Facades\Auth;
 
 class TemplateController extends Controller
 {
-    public function create(Request $request)
-    {
-   //     if(!auth()->user()->hasRole('admin')){
-   //         return 'you are not admin';
-   //     }
-    $template = new Template();
-        $template->name = $request->name;
-        $template->title = $request->title;
-        $template->data = $request->data;
-        $template->type = 'template';
-        $template->author = $request->author; // связано ли с auth()->user()?
-        // $template->parent = $request->parent;
-    $template->save();
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function index( Request $request )
+  {
+    $templates = Template::all()->toArray();
+    return \response()->json( $templates );
+  }
 
-        return 'template created';
-        //return view('home');
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function store( Request $request )
+  {
+    //
+    $template = new Template( $request->toArray() );
+    $template->user_id  = Auth::user()->id;
+    if( $template->save() ){
+      return \response()->json( ['message'=>'Success'] );
+    }
+    return \response()->json( ['message'=>'Error Save'], 500);
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Constructor\Template $template
+   * @return \Illuminate\Http\Response
+   */
+  public function show( Template $template )
+  {
+    return response()->json( $template->toArray() );
+
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Constructor\Template $template
+   * @return \Illuminate\Http\Response
+   */
+  public function edit( Template $template )
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @param  \App\Constructor\Template $template
+   * @return \Illuminate\Http\Response
+   */
+  public function update( Request $request, Template $template )
+  {
+    $old_template = $template;
+
+    if ( ! $old_template ) {
+      return response()->json( trans( "responses.not_found.template" ), 404, [], JSON_UNESCAPED_UNICODE );
     }
 
-    public function update(Request $request, $id)
-    {
-   //     if(!auth()->user()->hasRole('admin')){
-   //         return 'you are not admin';
-   //     }
+    $review = new Template( $old_template->toArray() );
+    $review->parent_template = $old_template->id;
+    $review->type = 'review';
+    if ( ! $review->save() ) {
+      return response()->json( trans( "responses.dberror" ), 400, [], JSON_UNESCAPED_UNICODE );
+    }
+    $old_template->name = $request->name;
+    $old_template->title = $request->title;
+    $old_template->data = json_encode( $request->data );
+    $old_template->type = 'template'; //1
+    $old_template->user_id = auth()->user()->id;
 
-        $parentTemplate = Template::find($id);
-    $template = new Template();
-        $template->name = $request->name;
-        $template->title = $request->title;
-        $template->data = $parentTemplate->data;
-        $template->type = 'revision';
-        $template->author = $parentTemplate->author; // связано ли с auth()->user()?
-        $template->parent = $parentTemplate->id;
-    $template->save();
-
-        return 'template updated';
-      //  return view('home');
+    if ( $old_template->save() ) {
+      return response()->json( $old_template, 200, [], JSON_UNESCAPED_UNICODE );
     }
 
-    public function templates()
-    {
-        //     if(!auth()->user()->hasRole('admin')){
-        //         return 'you are not admin';
-        //     }
-        $templates = Template::all();
+    return response()->json( trans( "responses.dberror" ), 400, [], JSON_UNESCAPED_UNICODE );
+  }
 
-        dd($templates);
-        return 'template';
-        //return view('home');
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Constructor\Template $template
+   * @return \Illuminate\Http\JsonResponse
+   * @throws \Exception
+   */
+  public function destroy( Template $template )
+  {
+    //
+    if( $template->delete() ){
+      return \response()->json( ['message'=>'Success'] );
     }
+    return \response()->json( ['message'=>'Error Delete'], 500 );
+  }
 
-    public function template($id)
-    {
-   //     if(!auth()->user()->hasRole('admin')){
-   //         return 'you are not admin';
-   //     }
-        $template = Template::find($id);
-
-        $templates = Template::where('parent', $template->id)->get();
-        /*
-        $templates_ids = [];
-        while($template->parent){
-            $template = Template::find($template->parent);
-            $templates_ids[] = $template->id;
-        }
-        $templates = Template::find($templates_ids);
-        */
-        dd($templates);
-        return 'template created';
-        //return view('home');
-    }
-
+  /**
+   * @param string $template_id
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function reviews( $template_id ){
+    $reviews = Template::where( 'parent_template', $template_id )
+      ->where( 'type', 'review' )
+      ->get()->toArray();
+    return \response()->json( $reviews );
+  }
 }
