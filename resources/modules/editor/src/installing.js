1ea1f2531771bbfd7bca1821c161ef2a68cc175b
shtempel <o_gredi@mail.ru>
2020-05-13 14:17:37 +0300

    installing add

---
+++ resources/modules/editor/src/installing.js
@@ -0,0 +1,28 @@
+import * as ace from "ace-builds";
+/**
+ * ACE Settings
+ * */
+
+ace.config.set('basePath', '/addons/ace');
+
+let langTools = ace.require('ace/ext/language_tools');
+console.log(langTools);
+
+var selectorCompleter = {
+  getCompletions: function getCompletions(editor, session, pos, prefix, callback) {
+    var list = [],
+        token = session.getTokenAt(pos.row, pos.column);
+
+    if (0 < prefix.length && 'selector'.match(prefix) && 'constant' === token.type) {
+      list = [{
+        name: 'selector',
+        value: 'selector',
+        score: 1,
+        meta: 'Altrp'
+      }];
+    }
+
+    callback(null, list);
+  }
+};
+langTools.addCompleter(selectorCompleter);
