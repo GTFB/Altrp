7555a1216928502a18763e3895d55a82926e1370
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-05 11:41:42 +1200

    Added wysiwyg controller based on textarea

---
+++ resources/modules/editor/src/js/components/controllers/WysisygController.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import DynamicIcon from '../../../svgs/dynamic.svg';
+import controllerDecorate from '../../decorators/controller';
+import ReactQuill from 'react-quill';
+
+class WysiwygController extends Component {
+  constructor(props){
+    super(props);
+    this.changeValue = this.changeValue.bind(this);
+    let value = this.props.currentElement.getSettings(this.props.controlId);
+    if(value === null && this.props.default){
+      value = this.props.default ;
+    }
+    value = value || '';
+    this.state = {value};
+    controllerDecorate(this);
+  }
+  changeValue(e){
+    this._changeValue(e.target.value);
+  }
+  getDefaultValue(){
+    return '';
+  }
+  render(){
+
+    return <div className="controller-container controller-container_wysiwyg">
+      <div className="controller-container__label">
+        {this.props.label}
+      </div>
+      <div className="controller-container__dynamic">
+        Dynamic
+        <DynamicIcon/>
+      </div>
+      <ReactQuill className="controller-container__wysiwyg" theme="snow" onChange={this.changeValue} value={this.state.value} />
+    </div>
+  }
+}
+
+function mapStateToProps(state) {
+  return{
+    currentElement:state.currentElement.currentElement,
+  };
+}
+export default connect(mapStateToProps)(WysiwygController);
