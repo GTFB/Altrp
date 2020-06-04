65908e9eb7112e52783b8353702a88a4f6f6ff43
shtempel <o_gredi@mail.ru>
2020-06-04 15:17:33 +0300

    admin only after auth

--- routes/web.php
+++ routes/web.php
@@ -18,29 +18,17 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-// templates
-/*Route::get('/admin/ajax/templates', 'TemplateController@templates')->name('templates');
-Route::get('/admin/ajax/templates/{id}', 'TemplateController@template')->name('template');
-Route::post('/admin/ajax/templates', 'TemplateController@create')->name('createTemplate');
-Route::put('/admin/ajax/templates', 'TemplateController@update')->name('updateTemplate');
-Route::delete('/admin/ajax/templates', 'TemplateController@delete')->name('deleteTemplate');*/
-
 Route::get( '/admin/editor', function (){
   return view( 'editor' );
-} )->name('editor');
+} )->middleware( 'auth' )->name('editor');
 Route::get( '/admin/editor-content', function (){
   return view( 'editor-content' );
-} )->name('editor-content');
+} )->middleware( 'auth' )->name('editor-content');
 
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth',], function () {
 
   Route::group(['prefix' => 'ajax'], function () {
-//    Route::get('/templates', "Constructor\Templates@getTemplates");
-//    Route::get('/templates/{template}', "Constructor\Templates@getTemplate");
-//    Route::post('/templates', "Constructor\Templates@insert");
-//    Route::put('/templates/{template}', "Constructor\Templates@update");
-//    Route::delete('/templates/{template}', "Constructor\Templates@delete");
 
     Route::get('/global-elements', "Constructor\GlobalElements@getElements");
     Route::get('/global-elements/{element}', "Constructor\GlobalElements@getElement");
@@ -59,6 +47,7 @@ Route::group(['prefix' => 'admin'], function () {
 
 Route::view('/admin/{path?}', 'admin')
   ->where('path', '.*')
+  ->middleware( 'auth' )
   ->name('admin');
 
 /**
@@ -83,8 +72,8 @@ foreach ( $frontend_routes as $frontend_route ) {
  * AJAX routes for frontend
 */
 
-Route::group(['prefix' => 'ajax'], function(){
+Route::group( ['prefix' => 'ajax'], function(){
 
   Route::resource( 'routes', 'Frontend\RouteController' );
 
-});
+} );
