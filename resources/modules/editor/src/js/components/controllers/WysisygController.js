294b6e849c63340871c26508e33592ad6fa0ecb6
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-06-08 17:51:53 +1200

    refactoring

--- resources/modules/editor/src/js/components/controllers/WysisygController.js
+++ resources/modules/editor/src/js/components/controllers/WysisygController.js
@@ -1,45 +1,49 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import DynamicIcon from '../../../svgs/dynamic.svg';
-import controllerDecorate from '../../decorators/controller';
-import ReactQuill from 'react-quill';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import DynamicIcon from "../../../svgs/dynamic.svg";
+import controllerDecorate from "../../decorators/controller";
+import ReactQuill from "react-quill";
 
 class WysiwygController extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.changeValue = this.changeValue.bind(this);
     let value = this.props.currentElement.getSettings(this.props.controlId);
-    if(value === null && this.props.default){
-      value = this.props.default ;
+    if (value === null && this.props.default) {
+      value = this.props.default;
     }
-    value = value || '';
-    this.state = {value};
+    value = value || "";
+    this.state = { value };
     controllerDecorate(this);
   }
-  changeValue(e){
-    this._changeValue(e.target.value);
+  changeValue(value) {
+    this._changeValue(value);
   }
-  getDefaultValue(){
-    return '';
+  getDefaultValue() {
+    return "";
   }
-  render(){
-
-    return <div className="controller-container controller-container_wysiwyg">
-      <div className="controller-container__label">
-        {this.props.label}
+  render() {
+    return (
+      <div className="controller-container controller-container_wysiwyg">
+        <div className="controller-container__label">{this.props.label}</div>
+        <div className="controller-container__dynamic">
+          Dynamic
+          <DynamicIcon />
+        </div>
+        <ReactQuill
+          className="controller-container__wysiwyg"
+          theme="snow"
+          onChange={this.changeValue}
+          value={this.state.value}
+        />
       </div>
-      <div className="controller-container__dynamic">
-        Dynamic
-        <DynamicIcon/>
-      </div>
-      <ReactQuill className="controller-container__wysiwyg" theme="snow" onChange={this.changeValue} value={this.state.value} />
-    </div>
+    );
   }
 }
 
 function mapStateToProps(state) {
-  return{
-    currentElement:state.currentElement.currentElement,
+  return {
+    currentElement: state.currentElement.currentElement,
   };
 }
 export default connect(mapStateToProps)(WysiwygController);
