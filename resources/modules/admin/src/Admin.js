afab99d8802036ca764eb2b6a19da17ed9a53616
shtempel <o_gredi@mail.ru>
2020-06-12 14:34:00 +0300

    add bars button

--- resources/modules/admin/src/Admin.js
+++ resources/modules/admin/src/Admin.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './sass/admin-style.scss';
 import {hot} from "react-hot-loader";
 import AdminLogo from './svgs/admin__logo.svg';
+import Bars from './svgs/bars.svg';
 import AllPages from './components/AllPages';
 import AdminSettings from './components/AdminSettings';
 import AssetSvg from './svgs/assets.svg';
@@ -41,8 +42,10 @@ class Admin extends Component {
     this.state = {
       adminState: {
         adminEnable: true,
-      }
+      },
+      pagesMenuShow: false,
     };
+    this.toggleMenu = this.toggleMenu.bind(this);
   }
 
   componentDidMount(){
@@ -58,10 +61,18 @@ class Admin extends Component {
       return{...state,adminState:{...adminState}}
     })
   }
+  toggleMenu(){
+    this.setState(state=>{
+      return {...state, pagesMenuShow: !state.pagesMenuShow}
+    })
+  }
   render() {
     let adminClasses = ['admin'];
     if(!this.state.adminState.adminEnable){
       adminClasses.push('pointer-event-none');
+    }
+    if(this.state.pagesMenuShow){
+      adminClasses.push('admin_pages-show');
     }
     return <Provider store={store}>
       <div className={adminClasses.join(' ')}>
@@ -69,6 +80,7 @@ class Admin extends Component {
           <nav className="admin-nav">
             <div className="admin-nav-top">
               <AdminLogo/>
+              <Bars className="admin__bars" onClick={this.toggleMenu}/>
               <ul className="admin-nav-list">
                 <li>
                   <Link to="/admin/dashboard" className="admin-nav-list__link">
@@ -118,6 +130,8 @@ class Admin extends Component {
                     <span>Settings</span>
                   </Link>
                 </li>
+              </ul>
+              <ul className="admin-nav-list admin-nav-list_pages">
                 <li>
                   <Link to="/admin/pages" className="admin-nav-list__link">
                     <PagesSvg className="icon"/>
