b5029d0a5e8c53f5c944459f9fa1e0c1bfc1f470
LLIyMu <52199092+LLIyMu@users.noreply.github.com>
2020-06-13 12:32:19 +0500

    Доделал вёрстку страницы юзеров

--- resources/modules/admin/src/components/Users.js
+++ resources/modules/admin/src/components/Users.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import PluginSvg from "../svgs/plugins.svg";
 import VectorSvg from '../svgs/vector.svg';
+import UserSvg from '../svgs/user.svg';
 
 
 export default class Users extends Component{
@@ -43,19 +44,74 @@ export default class Users extends Component{
             <div className="admin-users-table">
                 <table className="table">
                     <thead className="admin-users-table-head">
-                    <tr className="admin-table-row">
-                        <td className="admin-table__td admin-table__td_check">
-                            <input className="input-users" type="checkbox"/>Username<VectorSvg className="users-svg"/>
-                        </td>
-                        <td className="admin-table__td ">Name</td>
-                        <td className="admin-table__td ">Email</td>
-                        <td className="admin-table__td ">Role<VectorSvg className="users-svg"/></td>
-                        <td className="admin-table__td ">Post</td>
-                        <td className="admin-table__td ">Last Enter</td>
-                        <td className="admin-table__td ">Status</td>
-                    </tr>
+                        <tr className="admin-table-row">
+                            <td className="admin-table__td admin-table__td_check">
+                                <input className="input-users" type="checkbox"/>
+                                Username
+                                <VectorSvg className="vector-svg"/>
+                            </td>
+                            <td className="admin-table__td ">Name</td>
+                            <td className="admin-table__td ">Email</td>
+                            <td className="admin-table__td ">Role<VectorSvg className="vector-svg role-svg"/></td>
+                            <td className="admin-table__td ">Post</td>
+                            <td className="admin-table__td ">Last Enter</td>
+                            <td className="admin-table__td ">Status</td>
+                        </tr>
                     </thead>
-                    <tbody className="admin-table-body"></tbody>
+                    <tbody className="admin-table-body">
+                        <tr className="admin-table-row">
+                            <td className="admin-table__td admin-table__td_check ">
+                                <input className="input-users" type="checkbox"/>
+                                <UserSvg className="users-svg"/>
+                                <a>Adm1nCRMsyst</a>
+                            </td>
+                            <td className="admin-table__td ">Jora Beardman</td>
+                            <td className="admin-table__td "><a>20010810@academ-x.ru</a></td>
+                            <td className="admin-table__td "><a>Administrator</a></td>
+                            <td className="admin-table__td ">2</td>
+                            <td className="admin-table__td ">2020/02/17<br/> Last modified</td>
+                            <td className="admin-table__td ">Enable</td>
+                        </tr>
+                        <tr className="admin-table-row">
+                            <td className="admin-table__td admin-table__td_check">
+                                <input className="input-users" type="checkbox"/>
+                                <UserSvg className="users-svg"/>
+                                <a>Max1mEdit0r</a>
+                            </td>
+                            <td className="admin-table__td ">Maxim Ivanov</td>
+                            <td className="admin-table__td "><a>awpmi@academ-x.ru</a></td>
+                            <td className="admin-table__td "><a>Editor</a></td>
+                            <td className="admin-table__td ">3</td>
+                            <td className="admin-table__td ">2020/02/01<br/> Last modified</td>
+                            <td className="admin-table__td ">Enable</td>
+                        </tr>
+                        <tr className="admin-table-row">
+                            <td className="admin-table__td admin-table__td_check">
+                                <input className="input-users" type="checkbox"/>
+                                <UserSvg className="users-svg"/>
+                                <a>Dm1tr1yEdit0r</a>
+                            </td>
+                            <td className="admin-table__td ">Dmitriy Ancupov</td>
+                            <td className="admin-table__td "><a>awpda@academ-x.ru</a></td>
+                            <td className="admin-table__td "><a>Editor</a></td>
+                            <td className="admin-table__td ">1</td>
+                            <td className="admin-table__td ">2020/02/05<br/> Last modified</td>
+                            <td className="admin-table__td ">Enable</td>
+                        </tr>
+                        <tr className="admin-table-row">
+                            <td className="admin-table__td admin-table__td_check">
+                                <input className="input-users" type="checkbox"/>
+                                <UserSvg className="users-svg"/>
+                                <a>Sof14Edit0r</a>
+                            </td>
+                            <td className="admin-table__td ">Sofia Anpilova</td>
+                            <td className="admin-table__td "><a>awpsa@academ-x.ru</a></td>
+                            <td className="admin-table__td "><a>Editor</a></td>
+                            <td className="admin-table__td ">1</td>
+                            <td className="admin-table__td ">2020/02/25<br/> Last modified</td>
+                            <td className="admin-table__td ">Enable</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
