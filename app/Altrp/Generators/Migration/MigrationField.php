1efa76cab245ae1c83f6e3f859bd87b9ddba1233
523 <523@talp.ru>
2020-07-22 15:06:31 +0400

    баг при создании миграции default

--- app/Altrp/Generators/Migration/MigrationField.php
+++ app/Altrp/Generators/Migration/MigrationField.php
@@ -153,7 +153,7 @@ class MigrationField{
         
         $default = "";
         
-        if(isset($this->column->default) && !$this->checkAttribute("default")) {
+        if(isset($this->column->default) && $this->column->default !== "" && !$this->checkAttribute("default")) {
             $default = "->default('".$this->column->default."')";
         }
         
