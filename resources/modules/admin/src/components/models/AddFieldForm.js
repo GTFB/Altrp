c601f4c7cd2acfa38d635f3ee6750568bab9a8a4
Roma Kutischev <rukkola2@gmail.com>
2020-07-27 15:19:04 +0300

    add-field form for calculated fields layout

--- resources/modules/admin/src/components/models/AddFieldForm.js
+++ resources/modules/admin/src/components/models/AddFieldForm.js
@@ -47,20 +47,20 @@ class AddFieldForm extends Component {
 
   render() {
     return <form className="admin-form field-form" onSubmit={this.submitHandler}>
-      <div className="form-group">
-        <label htmlFor="field-title">Field Title</label>
-        <input type="text" id="field-title" required
-          value={this.state.value.title}
-          onChange={e => { this.changeValue(e.target.value, 'title') }}
+      {this.state.value.type !== 'calculated' && <div className="form-group">
+        <label htmlFor="field-name">Field Name</label>
+        <input type="text" id="field-name" required
+          value={this.state.value.name}
+          onChange={e => { this.changeValue(e.target.value, 'name') }}
           className="form-control" />
-      </div>
+      </div>}
 
       <div className="form-group__inline-wrapper">
         <div className="form-group form-group_width30">
-          <label htmlFor="field-name">Field Name</label>
-          <input type="text" id="field-name" required
-            value={this.state.value.name}
-            onChange={e => { this.changeValue(e.target.value, 'name') }}
+          <label htmlFor="field-title">Field Title</label>
+          <input type="text" id="field-title" required
+            value={this.state.value.title}
+            onChange={e => { this.changeValue(e.target.value, 'title') }}
             className="form-control" />
         </div>
 
@@ -70,23 +70,6 @@ class AddFieldForm extends Component {
             value={this.state.value.description}
             onChange={e => { this.changeValue(e.target.value, 'description') }}
             className="form-control" />
-        </div>
-      </div>
-
-      <div className="checkbox-group">
-        <div className="form-group">
-          <input type="checkbox" id="field-is_label"
-            checked={this.state.value.is_label}
-            onChange={e => { this.changeValue(e.target.checked, 'is_label') }}
-          />
-          <label className="checkbox-label" htmlFor="field-is_label">As Label</label>
-        </div>
-        <div className="form-group">
-          <input type="checkbox" id="field-is_title" required
-            checked={this.state.value.is_title}
-            onChange={e => { this.changeValue(e.target.checked, 'is_title') }}
-          />
-          <label className="checkbox-label" htmlFor="field-is_title">As Title</label>
         </div>
       </div>
 
@@ -105,93 +88,159 @@ class AddFieldForm extends Component {
         </select>
       </div>
 
-      <div className="form-group__inline-wrapper">
-        <div className="form-group form-group_width47">
-          <label htmlFor="field-length_value">Length/Value</label>
-          <input type="text" id="field-length_value" required
-            value={this.state.value.length_value}
-            onChange={e => { this.changeValue(e.target.value, 'length_value') }}
-            className="form-control" />
-        </div>
+      {this.state.value.type === 'calculated' ?
+        <>
+          <p className="centred">or Calculated Logic</p>
+          <p>if</p>
+          <div className="form-group">
+            <label>Field Name</label>
+            <select className="form-control"
+            >
+              <option disabled value="" />
+            </select>
+          </div>
+          <p>Comparison Operators</p>
 
-        <div className="form-group form-group_width47">
-          <label htmlFor="field-default">Default</label>
-          <input type="text" id="field-default" required
-            value={this.state.value.default}
-            onChange={e => { this.changeValue(e.target.value, 'default') }}
-            className="form-control" />
-        </div>
-      </div>
+          <div className="form-group">
+            <label>Not Null</label>
+            <select className="form-control"
+            >
+              <option disabled value="" />
+            </select>
+          </div>
 
-      <div className="form-group">
-        <label htmlFor="field-attribute">Attribute</label>
-        <select id="field-attribute" required
-          value={this.state.value.attribute}
-          onChange={e => { this.changeValue(e.target.value, 'attribute') }}
-          className="form-control"
-        >
-          <option disabled value="" />
-          {attributeOptions.map(item =>
-            <option key={item} value={item}>
-              {item}
-            </option>)}
-        </select>
-      </div>
+          <div className="form-group">
+            <label>Value</label>
+            <input type="text" className="form-control" />
+          </div>
 
-      <div className="form-group">
-        <label htmlFor="field-input_type">Input Type</label>
-        <select id="field-input_type" required
-          value={this.state.value.input_type}
-          onChange={e => { this.changeValue(e.target.value, 'input_type') }}
-          className="form-control"
-        >
-          <option disabled value="" />
-          {inputTipeOptions.map(item =>
-            <option key={item} value={item}>
-              {item}
-            </option>)}
-        </select>
-      </div>
+          <p>or</p>
 
-      {['select', 'checkbox', 'radio button'].includes(this.state.value.input_type) && <>
-        <div className="form-group">
-          <label htmlFor="field-options">Options</label>
-          <textarea id="field-options" required
-            value={this.state.value.options}
-            onChange={e => { this.changeValue(e.target.value, 'options') }}
-            className="form-control"
-          />
-        </div>
-        <p>Enter each choice on a new line.</p>
-        <p>Fro more control, you may specify both a value and label like this:</p>
-        <p>red | Red</p>
-      </>}
+          <div className="form-group">
+            <label>Field Name</label>
+            <select className="form-control"
+            >
+              <option disabled value="" />
+            </select>
+          </div>
 
-      <div className="checkbox-group">
-        <div className="form-group">
-          <input type="checkbox" id="field-nullable"
-            checked={this.state.value.nullable}
-            onChange={e => { this.changeValue(e.target.checked, 'nullable') }}
-          />
-          <label className="checkbox-label" htmlFor="field-nullable">Nullable</label>
-        </div>
-        <div className="form-group">
-          <input type="checkbox" id="field-indexed"
-            checked={this.state.value.indexed}
-            onChange={e => { this.changeValue(e.target.checked, 'indexed') }}
-          />
-          <label className="checkbox-label" htmlFor="field-indexed">Indexed</label>
-        </div>
-        <div className="form-group">
-          <input type="checkbox" id="field-editable"
-            checked={this.state.value.editable}
-            onChange={e => { this.changeValue(e.target.checked, 'editable') }}
-          />
-          <label className="checkbox-label" htmlFor="field-editable">Editable</label>
-        </div>
-      </div>
+          <p>Then = </p>
 
-      <div className="btn__wrapper">
+          <div className="form-group">
+            <label>Value</label>
+            <input type="text" className="form-control" />
+          </div>
+          <p>E.g. [Field Title]*[Field Title] + 10</p>
+          <button className="btn" type="button">+ Calculated Logic</button>
+        </> :
+        <>
+          <div className="checkbox-group">
+            <div className="form-group">
+              <input type="checkbox" id="field-is_label"
+                checked={this.state.value.is_label}
+                onChange={e => { this.changeValue(e.target.checked, 'is_label') }}
+              />
+              <label className="checkbox-label" htmlFor="field-is_label">As Label</label>
+            </div>
+            <div className="form-group">
+              <input type="checkbox" id="field-is_title" required
+                checked={this.state.value.is_title}
+                onChange={e => { this.changeValue(e.target.checked, 'is_title') }}
+              />
+              <label className="checkbox-label" htmlFor="field-is_title">As Title</label>
+            </div>
+          </div>
+
+          <div className="form-group__inline-wrapper">
+            <div className="form-group form-group_width47">
+              <label htmlFor="field-length_value">Length/Value</label>
+              <input type="text" id="field-length_value" required
+                value={this.state.value.length_value}
+                onChange={e => { this.changeValue(e.target.value, 'length_value') }}
+                className="form-control" />
+            </div>
+
+            <div className="form-group form-group_width47">
+              <label htmlFor="field-default">Default</label>
+              <input type="text" id="field-default" required
+                value={this.state.value.default}
+                onChange={e => { this.changeValue(e.target.value, 'default') }}
+                className="form-control" />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="field-attribute">Attribute</label>
+            <select id="field-attribute" required
+              value={this.state.value.attribute}
+              onChange={e => { this.changeValue(e.target.value, 'attribute') }}
+              className="form-control"
+            >
+              <option disabled value="" />
+              {attributeOptions.map(item =>
+                <option key={item} value={item}>
+                  {item}
+                </option>)}
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="field-input_type">Input Type</label>
+            <select id="field-input_type" required
+              value={this.state.value.input_type}
+              onChange={e => { this.changeValue(e.target.value, 'input_type') }}
+              className="form-control"
+            >
+              <option disabled value="" />
+              {inputTipeOptions.map(item =>
+                <option key={item} value={item}>
+                  {item}
+                </option>)}
+            </select>
+          </div>
+
+          {['select', 'checkbox', 'radio button'].includes(this.state.value.input_type) && <>
+            <div className="form-group">
+              <label htmlFor="field-options">Options</label>
+              <textarea id="field-options" required
+                value={this.state.value.options}
+                onChange={e => { this.changeValue(e.target.value, 'options') }}
+                className="form-control"
+              />
+            </div>
+            <p>Enter each choice on a new line.</p>
+            <p>Fro more control, you may specify both a value and label like this:</p>
+            <p>red | Red</p>
+          </>}
+
+          <div className="checkbox-group">
+            <div className="form-group">
+              <input type="checkbox" id="field-nullable"
+                checked={this.state.value.nullable}
+                onChange={e => { this.changeValue(e.target.checked, 'nullable') }}
+              />
+              <label className="checkbox-label" htmlFor="field-nullable">Nullable</label>
+            </div>
+            <div className="form-group">
+              <input type="checkbox" id="field-indexed"
+                checked={this.state.value.indexed}
+                onChange={e => { this.changeValue(e.target.checked, 'indexed') }}
+              />
+              <label className="checkbox-label" htmlFor="field-indexed">Indexed</label>
+            </div>
+            <div className="form-group">
+              <input type="checkbox" id="field-editable"
+                checked={this.state.value.editable}
+                onChange={e => { this.changeValue(e.target.checked, 'editable') }}
+              />
+              <label className="checkbox-label" htmlFor="field-editable">Editable</label>
+            </div>
+          </div>
+        </>
+      }
+
+
+      <div className="btn__wrapper btn_add">
         <button className="btn btn_success" type="submit">Add</button>
         <Link className="btn" to="/admin/models">Cancel</Link>
         {/* <button className="btn btn_failure">Delete</button> */}
