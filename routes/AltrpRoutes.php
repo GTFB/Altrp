f46a607ae43144d853a98470b4958bb6ab6c280e
shtempel <o_gredi@mail.ru>
2020-08-06 18:57:45 +0300

    deleted

--- routes/AltrpRoutes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-/**
- * File for user routes
- */
-Route::middleware('auth:api','auth')->resource('inner', 'AltrpControllers\InnerController');
-Route::put('inner/{inner}/{column}', 'AltrpControllers\InnerController@updateColumn');
