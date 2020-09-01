b56ed12ac62c8209320b2446428f977f11ada5b8
523 <523@talp.ru>
2020-09-01 17:44:02 +0400

    Расширеная фильтрация для SQLEditor

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Requests\ApiRequest;
+use Illuminate\Support\Str;
 
 /**
  * Get the script possible URL base
@@ -333,6 +334,8 @@ function selectForSQLEditor( $sql, $bindings, $sql_editor_params, ApiRequest $re
   }
   $_sql_and_filters = '';
   $_sql_filters = '';
+  $_sql_detail_filters = '';
+
   if( $request->get( 'filters' ) ){
     $_filters = json_decode( $request->get( 'filters' ), true );
 
@@ -348,10 +351,46 @@ function selectForSQLEditor( $sql, $bindings, $sql_editor_params, ApiRequest $re
         $_sql_and_filters .= ' AND `' . $key . '` LIKE "%' . $value . '%" ';
       }
     }
+    if( strpos( $sql, 'ALTRP_DETAIL_FILTERS' ) !== false || strpos( $sql, 'ALTRP_DETAIL_AND_FILTERS' ) !== false ) {
+      $_sql_detail_filters = '';
+      $_detail_filter = Str::after($sql, 'ALTRP_DETAIL_AND_FILTERS:');
+
+      if(strpos( $sql, 'ALTRP_DETAIL_FILTERS' ) !== false) {
+          $_sql_detail_filters = 'WHERE';
+          $_detail_filter = Str::after($sql, 'ALTRP_DETAIL_FILTERS:');
+      }
+
+      $_detail_filter = Str::before($_detail_filter, ' ');
+      $_detail_filter_array = explode(':',$_detail_filter);
+      $_detail_filter_params = [];
+
+      foreach ($_detail_filter_array as $param) {
+          $line = explode(',',$param);
+          $_detail_filter_params[$line[0]] = $line[1];
+      }
+
+      $i = 0;
+      foreach ( $_filters as $key => $value ) {
+         if(isset($_detail_filter_params[$key])) {
+             if($i != 0) {
+               $_sql_detail_filters .= ' AND ';
+             }
+             $_detail_filter_params[$key] = str_replace(".", "`.`", $_detail_filter_params[$key]);
+             $_sql_detail_filters .= ' `' . $_detail_filter_params[$key] . '` LIKE "%' . $value . '%" ';
+             $i++;
+         }
+      }
+    }
   }
   $sql = str_replace( 'ALTRP_FILTERS', $_sql_filters, $sql ) ;
 
   $sql = str_replace( 'ALTRP_AND_FILTERS', $_sql_and_filters, $sql ) ;
 
+  $sql = preg_replace( "/'?(ALTRP_DETAIL_FILTERS|ALTRP_DETAIL_AND_FILTERS)(:[a-z0-9_,.:]+)?'?/", $_sql_detail_filters, $sql ) ;
+
   return [ 'data' => DB::select( $sql, $bindings ) ];
+}
+
+function getFilterValues() {
+
 }
