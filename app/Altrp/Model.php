6113a65df9664b6e5f69cee74de9a332e46ec68a
shtempel <o_gredi@mail.ru>
2020-09-21 21:53:12 +0300

    delet model table check

--- app/Altrp/Model.php
+++ app/Altrp/Model.php
@@ -49,14 +49,14 @@ class Model extends EloquentModel
       if( self::where( 'name', $imported_model['name'] )->first() ){
         continue;
       }
-      $table = Table::where( 'name',  Arr::get( $imported_model, 'table_name' ) )->first();
-      if( ! $table ){
-        error_log( 'Не удалось сохранить модель ' . $imported_model['name'] .
-          ' таблица ' . $imported_model['table_name'] . ' не найдена!' );
-        continue;
-      }
+//      $table = Table::where( 'name',  Arr::get( $imported_model, 'table_name' ) )->first();
+//      if( ! $table ){
+//        error_log( 'Не удалось сохранить модель ' . $imported_model['name'] .
+//          ' таблица ' . $imported_model['table_name'] . ' не найдена!' );
+//        continue;
+//      }
       $new_model = new self( $imported_model );
-      $new_model->table_id = $table->id;
+//      $new_model->table_id = $table->id;
       $new_model->save();
     } 
   }
