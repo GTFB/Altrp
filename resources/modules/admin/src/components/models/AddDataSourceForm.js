23cde25942c180320e277a6d7b6067855d6586ff
Roma Kutischev <rukkola2@gmail.com>
2020-09-22 20:26:44 +0300

    data-source form refactoring

--- resources/modules/admin/src/components/models/AddDataSourceForm.js
+++ resources/modules/admin/src/components/models/AddDataSourceForm.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import AltrpSelect from "../altrp-select/AltrpSelect";
+import Resource from "../../../../editor/src/js/classes/Resource";
+import { titleToName } from "../../js/helpers";
 
-const typeOptions = ['resource', 'Get Queried', 'create', 'read', 'update', 'delete', 'options'];
+
+// const typeOptions = ['resource', 'Get Queried', 'create', 'read', 'update', 'delete', 'options'];
 const mockedOptions = [
   { value: 1, label: "Model title 1" },
   { value: 2, label: "Model title 2" },
@@ -16,26 +20,29 @@ class AddDataSourceForm extends Component {
       modelsOptions: mockedOptions,
       value: {
         title: '',
+        name: '',
         description: '',
-        type: [],
+        type: '',
+        request_type: '',
         model_id: '',
-        with: [],
-        route: '',
-        roles: []
+        url: '',
+        api_url: '',
+        auth: true,
+        access: {
+          roles: [],
+          permissions: []
+        }
       },
     };
     this.submitHandler = this.submitHandler.bind(this);
-    this.multipleSelectChangeHandler = this.multipleSelectChangeHandler.bind(this);
   }
 
   componentDidMount() {
-    // get: get: /admin/ajax/model_options .then(modelsOptions => {
-    //   this.setState({ modelsOptions });
-    // });
+    const resource = new Resource({ route: '/admin/ajax/model_options' });
+    resource.getAll().then(({ options }) => this.setState({ modelsOptions: options }));
   }
 
   changeValue(value, field) {
-    console.log(value)
     this.setState(state => {
       state = { ...state };
       state.value[field] = value;
@@ -43,110 +50,117 @@ class AddDataSourceForm extends Component {
     })
   }
 
-  multipleSelectChangeHandler({ target: { value, name } }) {
-    this.setState(state => {
-      const array = state.value[name];
-
-      if (array.includes(value)) {
-        const index = array.indexOf(value);
-        array.splice(index, 1);
-      } else {
-        array.push(value);
+  titleChangeHandler = e => {
+    e.persist();
+    this.setState(state => ({
+      ...state, value: {
+        ...state.value,
+        title: e.target.value,
+        name: titleToName(e.target.value)
       }
-      
-      return state;
-    })
+    }))
   }
+
+  /**
+   * Смена ролей
+   */
+  changeRoles = (roles) => {
+    let _roles = [];
+    if (roles) roles.forEach(r => {
+      _roles.push(r.value)
+    });
+    this.setState(state => {
+      const newState = _.cloneDeep(state);
+      newState.value.access.roles = _roles;
+      return newState;
+    });
+  };
+  /**
+   * Смена разрешений
+   */
+  changePermission = (permissions) => {
+    let _permissions = [];
+    if (permissions) permissions.forEach(p => {
+      _permissions.push(p.value)
+    });
+    this.setState(state => {
+      const newState = _.cloneDeep(state);
+      newState.value.access.permissions = _permissions;
+      return newState;
+    });
+  };
 
   submitHandler(e) {
     e.preventDefault();
-    // post: /admin/ajax/models (value)
+    const resource = new Resource({ route: '/admin/ajax/data_sources' });
+    resource.post(this.state.value);
     console.log(this.state.value);
   }
 
   render() {
     return <form className="admin-form" onSubmit={this.submitHandler}>
       <div className="form-group__inline-wrapper">
-        <div className="form-group form-group_width30">
+        <div className="form-group form-group_width47">
           <label htmlFor="data-source-title">Data Source Title</label>
           <input type="text" id="data-source-title" required
             value={this.state.value.title}
-            onChange={e => { this.changeValue(e.target.value, 'title') }}
+            onChange={this.titleChangeHandler}
             className="form-control" />
         </div>
 
-        <div className="form-group form-group_width65">
-          <label htmlFor="data-source-description">Data Source Description</label>
-          <input type="text" id="data-source-description" required
-            value={this.state.value.description}
-            onChange={e => { this.changeValue(e.target.value, 'description') }}
+        <div className="form-group form-group_width47">
+          <label htmlFor="data-source-title">Data Source Name</label>
+          <input type="text" id="data-source-title" required
+            value={this.state.value.name}
+            onChange={e => { this.changeValue(e.target.value, 'name') }}
             className="form-control" />
         </div>
       </div>
 
       <div className="form-group">
-        <label htmlFor="data-source-type">Data Source Type</label>
-        <select id="data-source-type" required multiple
-          name="type"
-          value={this.state.value.type}
-          onChange={this.multipleSelectChangeHandler}
-          className="form-control"
-        >
-          <option disabled value="" />
-          {typeOptions.map(item =>
-            <option key={item} value={item}>
-              {item}
-            </option>)}
-        </select>
+        <label htmlFor="data-source-description">Data Source Description</label>
+        <input type="text" id="data-source-description"
+          value={this.state.value.description}
+          onChange={e => { this.changeValue(e.target.value, 'description') }}
+          className="form-control" />
       </div>
 
       <div className="form-group__inline-wrapper">
         <div className="form-group form-group_width47">
-          <label htmlFor="data-source-model_id">Model</label>
-          <select id="data-source-model_id" required
-            value={this.state.value.model_id}
-            onChange={e => { this.changeValue(e.target.value, 'model_id') }}
+          <label htmlFor="data-source-type">Data Source Type</label>
+          <select id="data-source-type" required
+            name="type"
+            value={this.state.value.type}
+            onChange={e => { this.changeValue(e.target.value, 'type') }}
             className="form-control"
           >
             <option disabled value="" />
-            {this.state.modelsOptions.map(({ value, label }) =>
-              <option key={value} value={value}>
-                {label}
-              </option>)}
+            <option value="remote">remote</option>
           </select>
         </div>
 
         <div className="form-group form-group_width47">
-          <label htmlFor="data-source-with">With</label>
-          <select id="data-source-with" required multiple
-            name="with"
-            value={this.state.value.with}
-            onChange={this.multipleSelectChangeHandler}
+          <label htmlFor="data-source-type">Request Type</label>
+          <select id="data-source-type" required
+            name="type"
+            value={this.state.value.request_type}
+            onChange={e => { this.changeValue(e.target.value, 'request_type') }}
             className="form-control"
           >
             <option disabled value="" />
-            {this.state.modelsOptions.map(({ value, label }) =>
-              <option key={value} value={value}>
-                {label}
-              </option>)}
+            <option value="get">get</option>
+            <option value="post">post</option>
+            <option value="put">put</option>
+            <option value="delete">delete</option>
           </select>
         </div>
       </div>
 
       <div className="form-group">
-        <label htmlFor="data-source-route">Route</label>
-        <input type="text" id="data-source-route" required
-          value={this.state.value.route}
-          onChange={e => { this.changeValue(e.target.value, 'route') }}
-          className="form-control" />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="data-source-roles">Access</label>
-        <select id="data-source-roles" required multiple
-          name="roles"
-          value={this.state.value.roles}
-          onChange={this.multipleSelectChangeHandler}
+        <label htmlFor="data-source-model_id">Model</label>
+        <select id="data-source-model_id" required
+          value={this.state.value.model_id}
+          onChange={e => { this.changeValue(e.target.value, 'model_id') }}
           className="form-control"
         >
           <option disabled value="" />
@@ -156,6 +170,67 @@ class AddDataSourceForm extends Component {
             </option>)}
         </select>
       </div>
+
+      <div className="form-group__inline-wrapper">
+        <div className="form-group form-group_width47">
+          <label htmlFor="data-source-url">Route</label>
+          <input type="text" id="data-source-url" required
+            value={this.state.value.url}
+            onChange={e => { this.changeValue(e.target.value, 'url') }}
+            className="form-control" />
+        </div>
+
+        <div className="form-group form-group_width47">
+          <label htmlFor="data-source-api_url">API Route</label>
+          <input type="text" id="data-source-api_url" required
+            value={this.state.value.api_url}
+            onChange={e => { this.changeValue(e.target.value, 'api_url') }}
+            className="form-control" />
+        </div>
+      </div>
+
+      <h2 className="admin-form__subheader centred">Access</h2>
+
+      <input type="checkbox" id="field-auth"
+        checked={this.state.value.auth} value={this.state.value.auth}
+        onChange={e => { this.changeValue(e.target.checked, 'auth') }}
+      />
+      <label className="checkbox-label" htmlFor="field-auth">Auth</label>
+
+      {this.state.value.auth && <div className="form-group__inline-wrapper">
+        <div className="form-group col-6">
+          <label htmlFor="field-roles">Roles</label>
+          <AltrpSelect id="field-roles"
+            isMulti={true}
+            optionsRoute="/admin/ajax/role_options"
+            placeholder="All"
+            defaultOptions={[
+              {
+                value: null,
+                label: 'All',
+              }
+            ]}
+            value={this.state.value.roles}
+            onChange={this.changeRoles}
+          />
+        </div>
+        <div className="form-group col-6">
+          <label htmlFor="field-permissions">Permissions</label>
+          <AltrpSelect id="field-permissions"
+            isMulti={true}
+            optionsRoute="/admin/ajax/permissions_options"
+            placeholder="All"
+            defaultOptions={[
+              {
+                value: null,
+                label: 'All',
+              }
+            ]}
+            value={this.state.value.permissions}
+            onChange={this.changePermission}
+          />
+        </div>
+      </div>}
 
       <div className="btn__wrapper">
         <button className="btn btn_success" type="submit">Add</button>
