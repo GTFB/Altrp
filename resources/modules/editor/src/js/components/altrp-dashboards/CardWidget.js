b137ab697652aad892320c2001e07d3feeaddde9
chuikoffru <chuikoff.ru@gmail.com>
2020-09-28 12:41:43 +0300

    fixed icons

--- resources/modules/editor/src/js/components/altrp-dashboards/CardWidget.js
+++ resources/modules/editor/src/js/components/altrp-dashboards/CardWidget.js
@@ -3,10 +3,10 @@ import Card from "react-bootstrap/Card";
 import domtoimage from "dom-to-image";
 import ReactToPrint from "react-to-print";
 
-import GearFill from "react-bootstrap-icons/dist/icons/gear-fill";
-import TrashFill from "react-bootstrap-icons/dist/icons/trash-fill";
-import PrinterFill from "react-bootstrap-icons/dist/icons/printer-fill";
-import FileEarMark from "react-bootstrap-icons/dist/icons/file-earmark-arrow-down";
+import GearFill from "react-bootstrap-icons/dist/icons/sliders";
+import TrashFill from "react-bootstrap-icons/dist/icons/trash";
+import PrinterFill from "react-bootstrap-icons/dist/icons/printer";
+import FileEarMark from "react-bootstrap-icons/dist/icons/cloud-download";
 
 import EditWidget from "./EditWidget";
 import WidgetDiagram from "../../../../../admin/src/components/dashboard/WidgetDiagram";
