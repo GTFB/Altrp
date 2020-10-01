3f71a1440515c7656515be3129d5e8c1ced19aa0
Roma Kutischev <rukkola2@gmail.com>
2020-10-01 20:45:18 +0300

    show popup on inactivity

--- resources/modules/front-app/src/js/components/FrontPopup.js
+++ resources/modules/front-app/src/js/components/FrontPopup.js
@@ -7,33 +7,51 @@ class FrontPopup extends Component {
     isShownOnScroll: false
   }
 
-  // componentDidMount() {
-  //   const { on_page_load, on_click } = _.get(this.props, 'template.triggers.data', {});
+  componentDidMount() {
+    const { on_page_load, on_click, inactivity } = _.get(this.props, 'template.triggers.data', {});
 
-  //   if (on_page_load) {
-  //     setTimeout(() => this.setState({ isVisible: true }), on_page_load * 1000)
-  //   }
+    if (on_page_load || on_page_load === 0) {
+      setTimeout(() => this.setState({ isVisible: true }), on_page_load * 1000)
+    }
 
-  //   if (on_click) {
-  //     this.clickCounter = 0;
-  //     document.addEventListener('click', () => {
-  //       this.clickCounter += 1;
-  //       if (this.clickCounter === +on_click) {
-  //         this.clickCounter = 0;
-  //         this.setState({ isVisible: true });
-  //       }
-  //     })
-  //   }
-  // }
+    if (on_click) {
+      this.clickCounter = 0;
+      document.addEventListener('click', () => {
+        this.clickCounter += 1;
+        if (this.clickCounter === +on_click) {
+          this.clickCounter = 0;
+          this.setState({ isVisible: true });
+        }
+      })
+    }
 
-  // componentDidUpdate() {
-  //   const { on_scroll } = this.props.template.triggers.data;
-  //   const { isShownOnScroll } = this.state;
+    if (inactivity) {
+      this.inactivityTimeout = setTimeout(() => this.setState({ isVisible: true }), inactivity * 1000);
 
-  //   if (on_scroll && !isShownOnScroll && on_scroll.size <= this.props.topPosition * 100) {
-  //     this.setState({ isVisible: true, isShownOnScroll: true });
-  //   }
-  // }
+      this.resetTimer = () => {
+        clearTimeout(this.inactivityTimeout);
+        this.inactivityTimeout = setTimeout(() => this.setState({ isVisible: true }), inactivity * 1000);
+      }
+
+      const events = ['mousedown', 'keydown', 'touchstart'];
+      events.forEach(event => {
+        document.addEventListener(event, this.resetTimer, true);
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { on_scroll } = this.props.template.triggers.data;
+    const { isShownOnScroll } = this.state;
+
+    if (on_scroll && !isShownOnScroll && on_scroll.size <= this.props.topPosition * 100) {
+      this.setState({ isVisible: true, isShownOnScroll: true });
+    }
+
+    if (this.resetTimer && this.props.topPosition !== prevProps.topPosition) {
+      this.resetTimer();
+    }
+  }
 
   render() {
     const { isVisible } = this.state;
