4e986c86856a75f55584f6f01e5220a1442efa7d
Roma Kutischev <rukkola2@gmail.com>
2020-10-01 18:45:52 +0300

    commented popup triggering again

--- resources/modules/front-app/src/js/components/FrontPopup.js
+++ resources/modules/front-app/src/js/components/FrontPopup.js
@@ -7,33 +7,33 @@ class FrontPopup extends Component {
     isShownOnScroll: false
   }
 
-  componentDidMount() {
-    const { on_page_load, on_click } = _.get(this.props, 'template.triggers.data', {});
+  // componentDidMount() {
+  //   const { on_page_load, on_click } = _.get(this.props, 'template.triggers.data', {});
 
-    if (on_page_load) {
-      setTimeout(() => this.setState({ isVisible: true }), on_page_load * 1000)
-    }
+  //   if (on_page_load) {
+  //     setTimeout(() => this.setState({ isVisible: true }), on_page_load * 1000)
+  //   }
 
-    if (on_click) {
-      this.clickCounter = 0;
-      document.addEventListener('click', () => {
-        this.clickCounter += 1;
-        if (this.clickCounter === +on_click) {
-          this.clickCounter = 0;
-          this.setState({ isVisible: true });
-        }
-      })
-    }
-  }
+  //   if (on_click) {
+  //     this.clickCounter = 0;
+  //     document.addEventListener('click', () => {
+  //       this.clickCounter += 1;
+  //       if (this.clickCounter === +on_click) {
+  //         this.clickCounter = 0;
+  //         this.setState({ isVisible: true });
+  //       }
+  //     })
+  //   }
+  // }
 
-  componentDidUpdate() {
-    const { on_scroll } = this.props.template.triggers.data;
-    const { isShownOnScroll } = this.state;
+  // componentDidUpdate() {
+  //   const { on_scroll } = this.props.template.triggers.data;
+  //   const { isShownOnScroll } = this.state;
 
-    if (on_scroll && !isShownOnScroll && on_scroll.size <= this.props.topPosition * 100) {
-      this.setState({ isVisible: true, isShownOnScroll: true });
-    }
-  }
+  //   if (on_scroll && !isShownOnScroll && on_scroll.size <= this.props.topPosition * 100) {
+  //     this.setState({ isVisible: true, isShownOnScroll: true });
+  //   }
+  // }
 
   render() {
     const { isVisible } = this.state;
