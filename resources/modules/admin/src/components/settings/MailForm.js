3eeabb00bf0f12ceea6e2ba034bd6d766aba9ab7
Roma Kutischev <rukkola2@gmail.com>
2020-10-07 12:29:06 +0300

    mail form refactoring

--- resources/modules/admin/src/components/settings/MailForm.js
+++ resources/modules/admin/src/components/settings/MailForm.js
@@ -3,14 +3,14 @@ import Resource from "../../../../editor/src/js/classes/Resource";
 
 class MailForm extends Component {
   state = {
-    driver: 'smtp',
-    host: '',
-    port: '',
-    username: '',
-    password: '',
-    encryption: '',
-    from_address: '',
-    from_name: ''
+    mail_driver: 'smtp',
+    mail_host: '',
+    mail_port: '',
+    mail_username: '',
+    mail_password: '',
+    mail_encryption: '',
+    // mail_from_address: '',
+    // mail_from_name: ''
   };
 
   changeHandler = ({ target: { value, name } }) => {
@@ -24,13 +24,13 @@ class MailForm extends Component {
   };
 
   render() {
-    const { driver, host, port, username, password, encryption, from_address, from_name } = this.state;
+    const { mail_driver, mail_host, mail_port, mail_username, mail_password, mail_encryption/* , mail_from_address, mail_from_name */ } = this.state;
     return <form className="admin-form" onSubmit={this.submitHandler}>
       <div className="form-group">
-        <label htmlFor="driver">Driver</label>
-        <select id="driver" required
-          name="driver"
-          value={driver}
+        <label htmlFor="mail_driver">Driver</label>
+        <select id="mail_driver" required
+          name="mail_driver"
+          value={mail_driver}
           onChange={this.changeHandler}
           className="form-control"
         >
@@ -39,50 +39,50 @@ class MailForm extends Component {
       </div>
 
       <div className="form-group">
-        <label htmlFor="host">Host</label>
-        <input type="text" id="host" required
-          name="host"
-          value={host}
+        <label htmlFor="mail_host">Host</label>
+        <input type="text" id="mail_host" required
+          name="mail_host"
+          value={mail_host}
           onChange={this.changeHandler}
           className="form-control"
         />
       </div>
 
       <div className="form-group">
-        <label htmlFor="port">Port</label>
-        <input type="text" id="port" required
-          name="port"
-          value={port}
+        <label htmlFor="mail_port">Port</label>
+        <input type="text" id="mail_port" required
+          name="mail_port"
+          value={mail_port}
           onChange={this.changeHandler}
           className="form-control"
         />
       </div>
 
       <div className="form-group">
-        <label htmlFor="username">User Name</label>
-        <input type="text" id="username" required
-          name="username"
-          value={username}
+        <label htmlFor="mail_username">User Name</label>
+        <input type="text" id="mail_username" required
+          name="mail_username"
+          value={mail_username}
           onChange={this.changeHandler}
           className="form-control"
         />
       </div>
 
       <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" required
-          name="password"
-          value={password}
+        <label htmlFor="mail_password">Password</label>
+        <input type="password" id="mail_password" required
+          name="mail_password"
+          value={mail_password}
           onChange={this.changeHandler}
           className="form-control"
         />
       </div>
 
       <div className="form-group">
-        <label htmlFor="encryption">Encryption</label>
-        <select id="encryption" required
-          name="encryption"
-          value={encryption}
+        <label htmlFor="mail_encryption">Encryption</label>
+        <select id="mail_encryption" required
+          name="mail_encryption"
+          value={mail_encryption}
           onChange={this.changeHandler}
           className="form-control"
         >
@@ -92,25 +92,25 @@ class MailForm extends Component {
         </select>
       </div>
 
-      <div className="form-group">
-        <label htmlFor="from_address">From Address</label>
-        <input type="text" id="from_address" required
-          name="from_address"
-          value={from_address}
+      {/* <div className="form-group">
+        <label htmlFor="mail_from_address">From Address</label>
+        <input type="text" id="mail_from_address" required
+          name="mail_from_address"
+          value={mail_from_address}
           onChange={this.changeHandler}
           className="form-control"
         />
       </div>
 
       <div className="form-group">
-        <label htmlFor="from_name">From Name</label>
-        <input type="text" id="from_name" required
-          name="from_name"
-          value={from_name}
+        <label htmlFor="mail_from_name">From Name</label>
+        <input type="text" id="mail_from_name" required
+          name="mail_from_name"
+          value={mail_from_name}
           onChange={this.changeHandler}
           className="form-control"
         />
-      </div>
+      </div> */}
 
       <div className="btn__wrapper">
         <button className="btn btn_success" type="submit">Submit</button>
