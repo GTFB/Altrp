f2337f70aa8e47b8432a4546f3fddc1af85fcf6b
ilyasabushaev <522@talp.ru>
2020-10-15 16:43:15 +0400

    data

--- resources/modules/editor/src/js/components/altrp-dashboards/DataSourceDashboards.js
+++ resources/modules/editor/src/js/components/altrp-dashboards/DataSourceDashboards.js
@@ -1,5 +1,8 @@
 import { getDataByPath } from "../../../../../front-app/src/js/helpers";
 import GridLayout from 'react-grid-layout';
+import RGL, { WidthProvider } from "react-grid-layout";
+
+const ReactGridLayout = WidthProvider(RGL);
 
 import AddWidgetDataSource from './AddWidgetDataSource';
 
@@ -9,6 +12,14 @@ import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
 class DataSourceDashboards extends Component {
+
+  static defaultProps = {
+    className: "layout",
+    cols: 12,
+    rowHeight: 30,
+    onLayoutChange: function () { }
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -17,9 +28,10 @@ class DataSourceDashboards extends Component {
       containerWidth: Number(props.containerWidth),
       widgets: [],
       addMode: false,
-      settings: props.settings
+      settings: props.settings,
     };
-    console.log("SETTINGS !!! =>", this.state.settings);
+    this.onAddItem = this.onAddItem.bind(this);
+    this.onLayoutChange = this.onLayoutChange.bind(this);
   }
 
   /**
@@ -33,10 +45,25 @@ class DataSourceDashboards extends Component {
     }
   }
 
+  onLayoutChange(layout) {
+    this.setState({ layout });
+    this.props.onLayoutChange(layout);
+    console.log(this.state.layout);
+  }
+
   async addWidgetHandle(id, settings) {
-    let widgets = _.cloneDeep(this.state.widgets, []);
-    console.log(widgets);
-    console.log(settings);
+    // console.log(this.state.widgets);
+    // let widgets = typeof this.state.widgets !== 'undefined' && _.cloneDeep(this.state.widgets, []);
+    let widget = {
+      ...settings,
+      layout: {
+        ...settings.layout
+      }
+    }
+
+    console.log(widget);
+    // console.log(widgets);
+    // console.log(settings);
     // const data = {
     //   ...widget,
     //   title: title.current.value,
@@ -48,6 +75,27 @@ class DataSourceDashboards extends Component {
     //   onAdd(req.data);
     //   setIsShow(false);
     // }
+  }
+  onAddItem() {
+    console.log(123);
+  }
+  onBreakpointChange(breakpoint, cols) {
+    console.log(123);
+  }
+  onRemoveItem(i) {
+    console.log("removing", i);
+  }
+
+  resizeHandle(layout, oldLayoutItem, layoutItem, placeholder) {
+    console.log(layoutItem, placeholder);
+  }
+
+  createElement(widget) {
+    return <div key={widget.id} data-grid={widget.settings.layout || baseLayout}>
+      <div className="altrp-dashboard__card">
+        Виджет {widget.id}
+      </div>
+    </div>
   }
 
   render() {
@@ -68,16 +116,18 @@ class DataSourceDashboards extends Component {
     const baseLayout = { x: 0, y: 0, w: 3, h: 8, maxW: 12, minW: 3, maxH: 30, minH: 8 };
 
     return (
-      <GridLayout className="layout"
-        cols={12}
-        rowHeight={30}
+      <GridLayout
+        {...this.props}
+        layout={this.state.layout}
+        onLayoutChange={this.onLayoutChange}
         width={1010}>
         <div key="0" data-grid={baseLayout} >
           <div className="altrp-dashboard__card">
             {
               this.state.addMode ?
                 (
-                  <AddWidgetDataSource layout={baseLayout} dataSourceList={dataSources} addWidget={this.addWidgetHandle} />
+                  <AddWidgetDataSource
+                    resizeHandle={this.resizeHandle(this.state.layout, widget.settings)} layout={baseLayout} dataSourceList={dataSources} addWidget={this.addWidgetHandle} />
                 ) :
                 (
                   <button onClick={e => { this.setState({ addMode: true }) }} className="altrp-dashboard__card__add">
@@ -91,14 +141,8 @@ class DataSourceDashboards extends Component {
           </div>
         </div>
 
-        {this.state.widgets.map((widget) => (
-          <div key={widget.id} data-grid={widget.settings.layout || baseLayout}>
-            <div className="altrp-dashboard__card">
-              Виджет {widget.id}
-            </div>
-          </div>
-        ))}
-      </GridLayout >
+        {this.state.widgets.map((widget) => this.createElement(widget))}
+      </GridLayout>
     );
   }
 
