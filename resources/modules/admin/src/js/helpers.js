3aae7961acd5f35b03f5867df7ffe016b3c826df
Roma Kutischev <rukkola2@gmail.com>
2020-10-19 11:58:50 +0300

    buildPagesTree for multiple root pages

--- resources/modules/admin/src/js/helpers.js
+++ resources/modules/admin/src/js/helpers.js
@@ -120,12 +120,14 @@ function prepareTemplate(templateData, parent = null){
 export function buildPagesTree(pages) {
   const level = 0;
   const tree = [];
-  const root = pages.find(({ parent_page_id }) => parent_page_id === null);
+  const roots = pages.filter(({ parent_page_id }) => parent_page_id === null);
 
-  if (!root) return pages;
+  if (!roots.length) return pages;
 
-  tree.push(root);
-  treeRecursion(root.id, level + 1);
+  roots.forEach(root => {
+    tree.push(root);
+    treeRecursion(root.id, level + 1);
+  });  
 
   function treeRecursion(parentId, level) {
     const children = pages.filter(({ parent_page_id }) => parent_page_id === parentId);
