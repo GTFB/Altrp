d6d12a77a3a0cf7d1a35380a825cc9edbc9aca3a
ilyasabushaev <522@talp.ru>
2020-10-26 13:42:59 +0400

    fix

--- resources/views/front-app.blade.php
+++ resources/views/front-app.blade.php
@@ -23,9 +23,13 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta property="og:title" content="{{$title}}">
-  <title>{{ $title ?? 'altrp || FRONT APP'}}</title>
-
+  @isset($title)
+    <meta property="og:title" content="{{$title}}">
+    <title>{{ $title }}</title>
+  @else
+    <meta property="og:title" content="altrp || FRONT APP">
+    <title>altrp || FRONT APP</title>
+  @endisset
   <!-- Scripts -->
 
   <script>
