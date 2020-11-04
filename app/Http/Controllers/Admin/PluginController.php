70ce5896d33b48cbdda8d5a9de1b89ed9049152f
AkimovIgor <akimovigor5417@gmail.com>
2020-11-04 14:38:45 +0300

    fix plugin enabling/disabling + update composer.json

--- app/Http/Controllers/Admin/PluginController.php
+++ app/Http/Controllers/Admin/PluginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Nwidart\Modules\Facades\Module;
 
 
 class PluginController extends Controller
@@ -11,40 +13,69 @@ class PluginController extends Controller
 
       public function index(Request $request)
       {
-            $pluginsConfig = json_decode(file_get_contents(app_path() . "/Plugins/plugins.json"), true);
+          $modules = Module::all();
+          $modulesArr = [];
+          if ($modules) {
+              foreach ($modules as $module) {
+                  $modulesArr[] = [
+                      'name' => $module->getName(),
+                      'enabled' => $module->isEnabled(),
+                      'image' => $module->json()->image
+                  ];
+              }
+          }
+          return response()->json($modulesArr);
+//            $pluginsConfig = json_decode(file_get_contents(app_path() . "/Plugins/plugins.json"), true);
+//
+//            $pluginsInstalled = $pluginsConfig['installed'];
 
-            $pluginsInstalled = $pluginsConfig['installed'];
+//            $plugins = [];
 
-            $plugins = [];
+//            foreach ($pluginsInstalled as $plugin) {
+//                  $pluginFile = app_path() . "/Plugins/$plugin/$plugin.php";
+//                  $pluginClass = "App\\Plugins\\$plugin\\$plugin";
+//                  //Проверяем, есть ли основной класс
+//                  if (is_file($pluginFile)) {
+//                        $pluginInstance = new $pluginClass();
+//                        $plugins[] = [
+//                              'name' => $pluginInstance->getPluginName(),
+//                              'enabled' => $pluginInstance->pluginEnabled(),
+//                              'image' => $pluginInstance->getImage()
+//                        ];
+//                  }
+//            }
 
-            foreach ($pluginsInstalled as $plugin) {
-                  $pluginFile = app_path() . "/Plugins/$plugin/$plugin.php";
-                  $pluginClass = "App\\Plugins\\$plugin\\$plugin";
-                  //Проверяем, есть ли основной класс
-                  if (is_file($pluginFile)) {
-                        $pluginInstance = new $pluginClass();
-                        $plugins[] = [
-                              'name' => $pluginInstance->getPluginName(),
-                              'enabled' => $pluginInstance->pluginEnabled(),
-                              'image' => $pluginInstance->getImage()
-                        ];
-                  }
-            }
-
-            return response()->json($plugins);
+//            return response()->json($plugins);
       }
 
       public function switch(Request $request)
       {
-            $pluginName = $request->name;
+            $moduleName = $request->name;
             $value = $request->value;
-
-            $pluginClass = "App\\Plugins\\$pluginName\\$pluginName";
-            $pluginInstance = new $pluginClass();
+//            $pluginClass = "App\\Plugins\\$pluginName\\$pluginName";
+//            $pluginInstance = new $pluginClass();
+          $module = Module::has(ucfirst($moduleName));
+          if (! $module)
+              return response()->json('Module no found', 404);
+          $isEnabled = Module::isEnabled(ucfirst($moduleName));
+          $moduleInstance = Module::find(ucfirst($moduleName));
+          if ($isEnabled) {
+              $moduleInstance->disable();
+          } else {
+              $moduleInstance->enable();
+          }
 
             if ($value) {
-                  return response()->json(['message' => $pluginInstance->enable() ? "Plugin activated successfully" : "Plugin already activated"]);
+                  return response()->json([
+                      'message' => Module::isEnabled(ucfirst($moduleName))
+                          ? "Plugin activated successfully"
+                          : "Plugin already activated"
+                  ]);
             }
-            return response()->json(['message' => $pluginInstance->disable() ? "Plugin deactivated successfully" : "Plugin already deactivated"]);
+            return response()->json([
+                'message' => Module::isDisabled(ucfirst($moduleName))
+                    ? "Plugin deactivated successfully"
+                    : "Plugin already deactivated"
+            ]);
       }
 }
