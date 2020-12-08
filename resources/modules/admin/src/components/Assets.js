8f2807bcda814aa3980fb8231a9705048f08db9a
dashkin-dima <dashkin.dimitri@yandex.ru>
2020-12-08 17:54:37 +0300

    admin: assets: add tab

--- resources/modules/admin/src/components/Assets.js
+++ resources/modules/admin/src/components/Assets.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
+import { NavLink, withRouter } from "react-router-dom";
+
 import {iconsManager} from "../js/helpers";
 import Resource from "../../../editor/src/js/classes/Resource";
 
-
-
-export default class Assets extends Component {
+class Assets extends Component {
   constructor(props){
     super(props);
     this.onDragOver = this.onDragOver.bind(this);
@@ -12,13 +12,24 @@ export default class Assets extends Component {
     this.onDrop = this.onDrop.bind(this);
     this.onChange = this.onChange.bind(this);
     this.deleteClick = this.deleteClick.bind(this);
+    this.isActiveLink = this.isActiveLink.bind(this);
     this.state = {
       uploaderClasses: 'admin-assets__uploader uploader',
       assets: [],
+      filteredAssets: [],
       itemDeleteClasses: 'item__delete',
+      activeLink: '',
+    };
+    this.typesFiles = {
+      images: ['png', 'gif', 'jpg', 'jpeg'],
+      svgs: ['svg'],
+      fonts: ['ttf', 'woff2'],
+      archives: ['zip', 'rar'],
+      documents: ['doc', 'docx', 'xls', 'xlsx'],
     };
     this.resource  = new Resource({route: '/admin/ajax/media'});
   }
+  
   deleteClick(e){
     let assetId = e.currentTarget.dataset.assetid;
     this.setState(state=>{
@@ -53,11 +64,18 @@ export default class Assets extends Component {
     });
   }
   componentDidMount(){
+    const activeLink = this.changeUrlForTab();
+    
+    this.filterAssets(activeLink);
+
     this.resource.getAll().then(res=>{
       this.setState(state=>{
         return {...state, assets: res}
       })
     });
+  }
+  componentDidUpdate(){
+    this.changeUrlForTab();
   }
   onChange(e){
     this.updateAssets(e.target.files);
@@ -73,6 +91,47 @@ export default class Assets extends Component {
     this.setState(state=>{
       return {...state, uploaderClasses: 'admin-assets__uploader uploader'}
     });
+  }
+  isActiveLink(linkName) {
+    return (_, location) => {
+      const arrayPathname = location.pathname.split('/');
+      const currentLink = arrayPathname[arrayPathname.length - 1];
+      if(currentLink === linkName) return true;
+      return false;
+    }
+  }
+  onFilterAssets(activeLink) {
+    return () => this.filterAssets(activeLink);
+    
+  }
+  filterAssets(activeLink) {
+    const filteredAssets = this.state.assets.filter(asset => {
+      let typeFile = asset.url.split('.').pop();
+      if(activeLink==="others") {
+        for(const element in this.typesFiles) {
+          if(this.typesFiles[element].includes(typeFile)) return false;
+        }
+        return true;
+      } else {
+        return this.typesFiles[activeLink].includes(typeFile);
+      }
+    });
+    this.setState(state => {
+      return {...state, filteredAssets}
+    });
+  }
+  changeUrlForTab() {
+    const { location, history } = this.props;
+    this.path = location.pathname;
+    const arrayPathname = location.pathname.split('/');
+    if(arrayPathname[arrayPathname.length -1] === 'assets'){
+      history.push(`${this.path}/images`);
+      return 'images';
+    } else {
+      const activeLink = arrayPathname.pop();
+      this.path = arrayPathname.join('/');
+      return activeLink;
+    }
   }
   render() {
     let UploadIcon = iconsManager().getIconComponent('upload');
@@ -93,7 +152,6 @@ export default class Assets extends Component {
           <label className="uploader__label d-flex flex-column align-items-center">
             <UploadIcon width={100} height={100} className="icon"/>
             <input type="file"
-                   accept="image/*"
                    multiple={true}
                    onChange={this.onChange}
                    className="uploader__input"/>
@@ -102,12 +160,56 @@ export default class Assets extends Component {
             </span>
           </label>
         </div>
-        <div className="admin-assets__list p-4 mt-4 assets-list d-flex flex-wrap">
+        <div className="assets__tabs mt-4">
+          <NavLink 
+            className="assets__tab" 
+            activeClassName="assets__tab--selected"  
+            to={`${this.path}/images`}
+            isActive={this.isActiveLink('images')}
+            onClick={this.onFilterAssets('images')}
+          >IMAGES</NavLink>
+          <NavLink 
+            className="assets__tab" 
+            activeClassName="assets__tab--selected" 
+            to={`${this.path}/documents`}
+            isActive={this.isActiveLink('documents')}
+            onClick={this.onFilterAssets('documents')}
+          >DOCUMENTS</NavLink>
+          <NavLink 
+            className="assets__tab" 
+            activeClassName="assets__tab--selected" 
+            to={`${this.path}/fonts`}
+            isActive={this.isActiveLink('fonts')}
+            onClick={this.onFilterAssets('fonts')}
+          >FONTS</NavLink>
+          <NavLink 
+            className="assets__tab" 
+            activeClassName="assets__tab--selected" 
+            to={`${this.path}/svgs`}
+            isActive={this.isActiveLink('svgs')}
+            onClick={this.onFilterAssets('svgs')}
+          >SVGS</NavLink>
+          <NavLink 
+            className="assets__tab" 
+            activeClassName="assets__tab--selected" 
+            to={`${this.path}/archives`}
+            isActive={this.isActiveLink('archives')}
+            onClick={this.onFilterAssets('archives')}
+          >ARCHIVES</NavLink>
+          <NavLink 
+            className="assets__tab" 
+            activeClassName="assets__tab--selected" 
+            to={`${this.path}/others`}
+            isActive={this.isActiveLink('others')}
+            onClick={this.onFilterAssets('others')}
+          >OTHERS</NavLink>
+        </div>
+        <div className="admin-assets__list assets__tab-panel p-4 assets-list d-flex flex-wrap">
           {
-            this.state.assets.map(asset=>{
+            this.state.filteredAssets.map(asset=>{
               return<div className="assets-list__item item col-1" key={asset.id} >
                 <div className="item__background"
-                     style={{'backgroundImage': `url('${asset.url}')`}}/>
+                    style={{'backgroundImage': `url('${asset.url}')`}}/>
                 <button className={this.state.itemDeleteClasses}
                         data-assetid={asset.id}
                         title="Delete"
@@ -123,3 +225,5 @@ export default class Assets extends Component {
   }
 
 }
+
+export default withRouter(Assets);
