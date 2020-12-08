c933ca3c2c1809a5e28476fd45d7a8c94b77e1aa
shtempel <o_gredi@mail.ru>
2020-12-08 15:58:53 +0300

    update all media

--- database/migrations/2020_12_08_122541_media_type.php
+++ database/migrations/2020_12_08_122541_media_type.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Media;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,10 @@ class MediaType extends Migration
     //
     Schema::table( 'media', function ( Blueprint $table ){
       $table->string( 'type', 50 )->nullable()->index();
+    } );
+    Media::all()->each( function( Media $media ){
+      $media->type = 'image';
+      $media->save();
     } );
   }
 
