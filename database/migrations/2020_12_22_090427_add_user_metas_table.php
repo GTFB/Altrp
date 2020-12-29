ab80e11b730ea663911d39884db7d009912ecfb5
AkimovIgor <akimovigor5417@gmail.com>
2020-12-29 18:15:10 +0300

    fix conditions in the user meta migration

--- database/migrations/2020_12_22_090427_add_user_metas_table.php
+++ database/migrations/2020_12_22_090427_add_user_metas_table.php
@@ -16,45 +16,56 @@ class AddUserMetasTable extends Migration
      */
     public function up()
     {
-        if (Table::where([['name','user_metas'], ['preset',1]])->first()) {
-            return;
+        $table = Table::where([['name','user_metas']])->orWhere('name', 'usermeta')->first();
+        if (! $table) {
+            $table = new Table([
+                'name' => 'user_metas',
+                'title' => 'UserMeta',
+                'preset' => 1
+            ]);
+            Table::withoutEvents(function () use ($table) {
+                $table->save();
+            });
+        } else {
+            $table->update(['preset' => 1]);
         }
 
-        $table = new Table([
-            'name' => 'user_metas',
-            'title' => 'UserMeta',
-            'preset' => 1
-        ]);
-        Table::withoutEvents(function () use ($table) {
-            $table->save();
-        });
+        $model = Model::where([['name','user_meta']])->first();
+        if (! $model) {
+            $model = new Model([
+                'name' => 'user_meta',
+                'title' => 'UserMeta',
+                'namespace' => 'App\UserMeta',
+                'table_id' => $table->id,
+                'soft_deletes' => 0,
+                'time_stamps' => 0,
+                'pk' => 'id',
+                'preset' => 1,
+            ]);
+            Model::withoutEvents(function () use ($model) {
+                $model->save();
+            });
+        } else {
+            $model->update(['preset' => 1]);
+        }
 
+        $column = Column::where([['name', 'id'], ['table_id', $table->id]])->first();
+        if (! $column) {
+            $column = new Column([
+                'name' => 'id',
+                'title' => 'ID',
+                'type' => 'bigInteger',
+                'preset' => 1,
+                'table_id' => $table->id,
+                'indexed' => 1,
+            ]);
+            Column::withoutEvents(function () use ($column) {
+                $column->save();
+            });
+        } else {
+            $column->update(['preset' => 1]);
+        }
 
-        $model = new Model([
-            'name' => 'user_meta',
-            'title' => 'UserMeta',
-            'namespace' => 'App\UserMeta',
-            'table_id' => $table->id,
-            'soft_deletes' => 0,
-            'time_stamps' => 0,
-            'pk' => 'id',
-            'preset' => 1,
-        ]);
-        Model::withoutEvents(function () use ($model) {
-            $model->save();
-        });
-
-        $column = new Column([
-            'name' => 'id',
-            'title' => 'ID',
-            'type' => 'bigInteger',
-            'preset' => 1,
-            'table_id' => $table->id,
-            'indexed' => 1,
-        ]);
-        Column::withoutEvents(function () use ($column) {
-            $column->save();
-        });
     }
 
     /**
