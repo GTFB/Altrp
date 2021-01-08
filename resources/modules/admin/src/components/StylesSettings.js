4fad44d33777cc0a346cc0f48020ad098a472ef6
ilyasabushaev <522@talp.ru>
2021-01-08 14:33:39 +0400

    color scheme

--- resources/modules/admin/src/components/StylesSettings.js
+++ resources/modules/admin/src/components/StylesSettings.js
@@ -2,90 +2,131 @@ import React, { Component } from "react";
 import AutoUpdateInput from "./AutoUpdateInput";
 import { iconsManager } from "../js/helpers";
 import Resource from "../../../editor/src/js/classes/Resource";
-
+import ColorSchemeSettings from "./settings/ColorSchemeSettings";
 
 class StylesSettings extends Component {
   state = {
     iconFile: null,
-    uploaderClasses: 'admin-assets__uploader uploader',
-    itemDeleteClasses: 'item__delete',
+    uploaderClasses: "admin-assets__uploader uploader",
+    itemDeleteClasses: "item__delete"
   };
 
-  onDrop = (e) => {
+  onDrop = e => {
     e.persist();
     e.preventDefault();
     e.stopPropagation();
     this.setState(state => {
-      return { ...state, uploaderClasses: 'admin-assets__uploader uploader', iconFile: e.dataTransfer.files[0] }
-    })
-  }
+      return {
+        ...state,
+        uploaderClasses: "admin-assets__uploader uploader",
+        iconFile: e.dataTransfer.files[0]
+      };
+    });
+  };
 
-  onDragOver = (e) => {
+  onDragOver = e => {
     e.preventDefault();
     e.stopPropagation();
     this.setState(state => {
-      return { ...state, uploaderClasses: 'admin-assets__uploader uploader uploader_drag-over' }
+      return {
+        ...state,
+        uploaderClasses: "admin-assets__uploader uploader uploader_drag-over"
+      };
     });
-  }
+  };
 
-  onDragLeave = (e) => {
+  onDragLeave = e => {
     this.setState(state => {
-      return { ...state, uploaderClasses: 'admin-assets__uploader uploader' }
+      return { ...state, uploaderClasses: "admin-assets__uploader uploader" };
     });
-  }
+  };
 
   submitHandler = e => {
     e.preventDefault();
-    new Resource({ route: '/admin/ajax/favicon' })
+    new Resource({ route: "/admin/ajax/favicon" })
       .postFile(this.state.iconFile)
       .then(() => alert("Icon successfully  uploaded."));
-  }
+  };
 
   render() {
-    let UploadIcon = iconsManager().getIconComponent('upload');
+    let UploadIcon = iconsManager().getIconComponent("upload");
     const { iconFile } = this.state;
-    return <div className="admin-styles-settings">
-      <table>
-        <tbody className="admin-settings-table-row">
-          <tr className="admin-settings-table-row">
-            <td className="admin-settings-table__td row-text" width="10%">
-              <label htmlFor="settings-container-width">
-                Container Width
-              </label>
-            </td>
-            <td className="admin-settings-table__td">
-              <AutoUpdateInput type="number"
-                route="/admin/ajax/settings"
-                resourceid="container_width"
-                id="settings-container-width"
-                className="form__input w-25" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <form onSubmit={this.submitHandler}>
-        <legend>Add Favicon</legend>
-        <div className={this.state.uploaderClasses}
-          onDragLeave={this.onDragLeave}
-          onDrop={this.onDrop}
-          onDragOver={this.onDragOver}>
-          <label className="uploader__label d-flex flex-column align-items-center">
-            <UploadIcon width={100} height={100} className="icon" />
-            <input type="file"
-              accept="image/*"
-              onChange={e => this.setState({ iconFile: e.target.files[0] })}
-              className="uploader__input" />
-            <span className="uploader__text text text_bold">
-              Drag or Choose File
-            </span>
-          </label>
-        </div>
-        {iconFile && <div className="centred mt-2">
-          <button className="admin-settings-button btn btn-sm" type="submit">Upload icon</button>
-        </div>}
-      </form>
-    </div>
+    return (
+      <div className="admin-styles-settings">
+        <table>
+          <tbody className="admin-settings-table-row">
+            <tr className="admin-settings-table-row">
+              <td className="admin-settings-table__td row-text" width="10%">
+                <label htmlFor="settings-container-width">
+                  Container Width
+                </label>
+              </td>
+              <td className="admin-settings-table__td">
+                <AutoUpdateInput
+                  type="number"
+                  route="/admin/ajax/settings"
+                  resourceid="container_width"
+                  id="settings-container-width"
+                  className="form__input w-25"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <form onSubmit={this.submitHandler}>
+          <legend>Add Favicon</legend>
+          <div
+            className={this.state.uploaderClasses}
+            onDragLeave={this.onDragLeave}
+            onDrop={this.onDrop}
+            onDragOver={this.onDragOver}
+          >
+            <label className="uploader__label d-flex flex-column align-items-center">
+              <UploadIcon width={100} height={100} className="icon" />
+              <input
+                type="file"
+                accept="image/*"
+                onChange={e => this.setState({ iconFile: e.target.files[0] })}
+                className="uploader__input"
+              />
+              <span className="uploader__text text text_bold">
+                Drag or Choose File
+              </span>
+            </label>
+          </div>
+          {iconFile && (
+            <div className="centred mt-2">
+              <button
+                className="admin-settings-button btn btn-sm"
+                type="submit"
+              >
+                Upload icon
+              </button>
+            </div>
+          )}
+        </form>
+        <table>
+          <tbody className="admin-settings-table-row">
+            <tr className="admin-settings-table-row">
+              <td
+                className="admin-settings-table__td row-text pb-0"
+                width="10%"
+              >
+                <label htmlFor="settings-container-width">
+                  Color Scheme Pallet
+                </label>
+              </td>
+            </tr>
+            <tr className="admin-settings-table-row">
+              <td className="admin-settings-table__td row-text" width="10%">
+                <ColorSchemeSettings />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
-export default StylesSettings
+export default StylesSettings;
--- resources/modules/editor/src/js/components/altrp-map/AltrpMapConstructor.js
+++ resources/modules/editor/src/js/components/altrp-map/AltrpMapConstructor.js
@@ -34,54 +34,58 @@ function AltrpMapConstructor({ settings, id }) {
     longitude = lngDatasource;
   }
   const dynamicGeoObjectsRepeater = useMemo(() => {
-    return objects
-      .map(r => {
-        const geoObj = getDataByPath(r.path, []);
+    if (_.keys(objects).length > 0) {
+      return objects
+        .map(r => {
+          const geoObj = getDataByPath(r.path, []);
 
-        const result = Array.isArray(geoObj)
-          ? geoObj.map(data => ({
-              type: "Feature",
-              geometry: {
-                type: "Point",
-                coordinates: [
-                  Number(_.get(data, r.latitude)),
-                  Number(_.get(data, r.longitude))
-                ]
-              },
-              properties: {
-                fillOpacity: 1,
-                icon: r.icon || "GoogleMarker",
-                tooltip: r.tooltipByKeyboard
-                  ? r.tooltip
-                  : _.get(data, r.tooltip) || "",
-                popup: r.popupByKeyboard ? r.popup : _.get(data, r.popup) || "",
-                fillColor: r.color?.colorPickedHex || "#3388ff"
-              }
-            }))
-          : {
-              type: "Feature",
-              geometry: {
-                type: "Point",
-                coordinates: [
-                  Number(_.get(geoObj, r.latitude)),
-                  Number(_.get(geoObj, r.longitude))
-                ]
-              },
-              properties: {
-                fillOpacity: 1,
-                icon: r.icon || "GoogleMarker",
-                tooltip: r.tooltipByKeyboard
-                  ? r.tooltip
-                  : _.get(geoObj, r.tooltip) || "",
-                popup: r.popupByKeyboard
-                  ? r.popup
-                  : _.get(geoObj, r.popup) || "",
-                fillColor: r.color?.colorPickedHex || "#3388ff"
-              }
-            };
-        return result;
-      })
-      .flat();
+          const result = Array.isArray(geoObj)
+            ? geoObj.map(data => ({
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [
+                    Number(_.get(data, r.latitude)),
+                    Number(_.get(data, r.longitude))
+                  ]
+                },
+                properties: {
+                  fillOpacity: 1,
+                  icon: r.icon || "GoogleMarker",
+                  tooltip: r.tooltipByKeyboard
+                    ? r.tooltip
+                    : _.get(data, r.tooltip) || "",
+                  popup: r.popupByKeyboard
+                    ? r.popup
+                    : _.get(data, r.popup) || "",
+                  fillColor: r.color?.colorPickedHex || "#3388ff"
+                }
+              }))
+            : {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [
+                    Number(_.get(geoObj, r.latitude)),
+                    Number(_.get(geoObj, r.longitude))
+                  ]
+                },
+                properties: {
+                  fillOpacity: 1,
+                  icon: r.icon || "GoogleMarker",
+                  tooltip: r.tooltipByKeyboard
+                    ? r.tooltip
+                    : _.get(geoObj, r.tooltip) || "",
+                  popup: r.popupByKeyboard
+                    ? r.popup
+                    : _.get(geoObj, r.popup) || "",
+                  fillColor: r.color?.colorPickedHex || "#3388ff"
+                }
+              };
+          return result;
+        })
+        .flat();
+    }
   }, [objects, currentDataStorage]);
   // Сохраняем данные карты
   const handleSave = data => {
