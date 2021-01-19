22e6c15a5acd50d406f2344a68b4604ac9d28837
ilyasabushaev <522@talp.ru>
2021-01-19 16:57:36 +0400

    connect data from datasource to model

--- resources/modules/editor/src/js/components/altrp-map/MarkerCluster.js
+++ resources/modules/editor/src/js/components/altrp-map/MarkerCluster.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import PropTypes from "prop-types";
 import L from "leaflet";
 import "leaflet.markercluster/dist/leaflet.markercluster";
 import "leaflet.markercluster/dist/MarkerCluster.css";
@@ -15,41 +14,28 @@ const MarkerCluster = ({ markers }) => {
   let markersData = markers.map(item => {
     return {
       position: {
-        lat: item.geometry.coordinates[1],
-        lng: item.geometry.coordinates[0]
+        lat: item.feature?.geometry?.coordinates[1],
+        lng: item.feature?.geometry?.coordinates[0]
       },
-      popup: item.properties.popup,
-      popup: item.properties.tooltip,
-      icon: item.properties.icon
+      popup: item.feature?.properties?.popup,
+      tooltip: item.feature?.properties?.tooltip,
+      icon: item.feature?.properties?.icon
     };
   });
   useEffect(() => {
     mcg.clearLayers();
     markersData.forEach(({ position, tooltip, popup, icon }) => {
-      console.log(icon);
       L.marker(new L.LatLng(position.lat, position.lng), {
         icon: customIcon(icon, "#3388ff", [25, 41])
       })
         .addTo(mcg)
-        .bindPopup(popup);
+        .bindPopup(popup)
+        .bindTooltip(tooltip);
     });
-
-    // optionally center the map around the markers
-    // map.fitBounds(mcg.getBounds());
-    // // add the marker cluster group to the map
     map.addLayer(mcg);
   }, [markers, map]);
 
   return null;
-};
-
-MarkerCluster.propTypes = {
-  markers: PropTypes.arrayOf(
-    PropTypes.shape({
-      position: PropTypes.objectOf(PropTypes.number).isRequired
-      //   text: PropTypes.string.isRequired
-    }).isRequired
-  ).isRequired
 };
 
 export default MarkerCluster;
