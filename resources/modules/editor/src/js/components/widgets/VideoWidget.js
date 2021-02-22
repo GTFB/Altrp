15abf227678cbd9f8d945a5021d2c643d3c7e1c0
ilyasabushaev <522@talp.ru>
2021-02-22 17:33:43 +0400

    video from datasource

--- resources/modules/editor/src/js/components/widgets/VideoWidget.js
+++ resources/modules/editor/src/js/components/widgets/VideoWidget.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   getDataByPath,
   parseIDfromYoutubeURL
 } from "../../../../../front-app/src/js/helpers";
 class VideoWidget extends Component {
   render() {
-    const { is_youtube, youtube_id, content_path } = this.props.element.settings;
+    const {
+      is_youtube,
+      youtube_id,
+      content_path
+    } = this.props.element.settings;
     let dynamicID;
-
+    const url = getDataByPath(content_path);
     if (content_path) {
-      const youtubeURL = getDataByPath(content_path);
-      if (youtubeURL) dynamicID = parseIDfromYoutubeURL(youtubeURL);
+      if (url) dynamicID = parseIDfromYoutubeURL(url);
     }
 
-    return is_youtube ?
-      <iframe className="altrp-video" allowFullScreen src={`https://www.youtube.com/embed/${dynamicID || youtube_id}`} /> :
-      <video className="altrp-video" controls src={`https://www.youtube.com/embed/e4Ao-iNPPUc`} />
+    return is_youtube ? (
+      <iframe
+        className="altrp-video"
+        allowFullScreen
+        src={`https://www.youtube.com/embed/${dynamicID || youtube_id}`}
+      />
+    ) : (
+      <video className="altrp-video" controls src={url} />
+    );
   }
 }
 
