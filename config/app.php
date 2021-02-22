a5373667142f03661f478a8460c376843aafa8ff
shtempel <o_gredi@mail.ru>
2021-02-23 00:32:12 +0300

    new conditions

--- config/app.php
+++ config/app.php
@@ -33,7 +33,7 @@ return [
     |
     */
 
-    'altrp_version' => '0.13.10',
+    'altrp_version' => '0.13.11',
 
 
     /*
--- resources/modules/front-app/src/js/store/reducers.js
+++ resources/modules/front-app/src/js/store/reducers.js
@@ -18,6 +18,7 @@ import { exportDashboard } from "./altrp-dashboard-export/reducers";
 import {mediaScreenReducer} from "./media-screen-storage/reducers";
 import {currentTitleReducer} from "./current-title/reducers";
 import {currentEmailTemplateReducer} from "./current-email-template/reducers";
+import {currentPageReducer} from "./current-page/reducers";
 export default combineReducers({
   appRoutes: appRoutesReducer,
   currentModel: currentModelReducer,
@@ -38,4 +39,5 @@ export default combineReducers({
   currentScreen: mediaScreenReducer,
   currentTitle: currentTitleReducer,
   currentEmailTemplate: currentEmailTemplateReducer,
+  altrpPage: currentPageReducer,
 });
