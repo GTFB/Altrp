aff9722063902461dc3d98c1edcb7b491beca698
Sannikov Alexey <alex617180@mail.ru>
2021-02-27 15:13:03 +0500

    fix: delete old niotifications widget

--- resources/modules/editor/src/js/classes/modules/ElementsManager.js
+++ resources/modules/editor/src/js/classes/modules/ElementsManager.js
@@ -53,9 +53,6 @@ import TemplateWidget from "../../components/widgets/TemplateWidget";
 import Video from "../elements/Video";
 import VideoWidget from "../../components/widgets/VideoWidget";
 
-// Websocket import
-import Notifications from "../elements/Notifications";
-import NotificationsWidget from "../../components/widgets/NotificationsWidget";
 
 export default class ElementsManger {
   constructor() {
@@ -88,8 +85,6 @@ export default class ElementsManger {
     this.elements[Template.getName()] = Template;
     this.elements[ExportPanel.getName()] = ExportPanel;
     this.elements[Html.getName()] = Html;
-    // Websocket Notifications
-    this.elements[Notifications.getName()] = Notifications;
     this.elements[Video.getName()] = Video;
 
     //список компонентов
@@ -120,8 +115,6 @@ export default class ElementsManger {
     this.components[Template.getName()] = TemplateWidget;
     this.components[ExportPanel.getName()] = ExportPanelWidget;
     this.components[Html.getName()] = HtmlWidget;
-    // Websocket Notifications widget
-    this.components[Notifications.getName()] = NotificationsWidget;
     this.components[Video.getName()] = VideoWidget;
   }
 
