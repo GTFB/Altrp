036c8900fbdde8279b665e61de9931f500ebf5c3
mihey3000 <48413568+mihey3000@users.noreply.github.com>
2021-03-02 21:15:01 +0500

    romb-node style

--- resources/modules/robots/src/js/components/sidebar/nodes/Condition.js
+++ resources/modules/robots/src/js/components/sidebar/nodes/Condition.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Handle } from 'react-flow-renderer';
+import { Handle } from "react-flow-renderer";
 
 export default class Condition extends React.Component {
   constructor(props) {
@@ -7,25 +7,39 @@ export default class Condition extends React.Component {
   }
 
   render() {
-    let nodeClasses = "flow-node-condition";
-    if (this.props.selected) nodeClasses += " selected";    
-   
+    // let nodeClasses = "flow-node-condition";
+    let nodeClasses = "romb";
+    if (this.props.selected) nodeClasses += " selected";
+    const dataLabel = this.props?.data?.label;
+
+    const trimLabel = str => {
+      return str.slice(0, 65);
+    };
+
     return (
       <div className={nodeClasses}>
-        <Handle type="target" position="top" style={{ zIndex: 100}}/>
-        <div className="condition-romb"><h3>{this.props?.data?.label}</h3></div>
-        <div className="condition-type">{this.props?.type}</div>
+        <Handle
+          type="target"
+          position="top"
+          style={{ zIndex: 100, left: "0%" }}
+        />
+        <div>
+          <div className="condition-text">
+            <div>{trimLabel(dataLabel)}</div>
+            <div className="condition-type">{this.props?.type}</div>
+          </div>
+        </div>
         <Handle
           type="source"
           position="left"
           id="no"
-          style={{ top: '65%', borderRadius: 0 }}
+          style={{ top: "100%", borderRadius: 0 }}
         />
         <Handle
           type="source"
           position="right"
           id="yes"
-          style={{ top: '65%', borderRadius: 0 }}
+          style={{ top: "0%", borderRadius: 0 }}
         />
       </div>
     );
