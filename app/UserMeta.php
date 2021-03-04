c58a2d8f772b440e6c00a8c276d6b23bf8b81432
AkimovIgor <akimovigor5417@gmail.com>
2021-03-04 16:55:14 +0300

    add fillable attributes to user meta model

--- app/UserMeta.php
+++ app/UserMeta.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserMeta extends Model
 {
+    protected $table = 'user_metas';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'patronymic',
+        'user_id',
+        'company_id',
+    ];
+
     public $timestamps = false;
 }
