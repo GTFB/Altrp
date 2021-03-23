4123930c47701bf41e5f4bda30e302d23ac374d3
SamoiIenko <samoilenkoand@gmail.com>
2021-03-23 11:44:17 +0300

    add new NCS for dashboard; edit custom tooltip

--- resources/modules/admin/src/components/dashboard/widgets/d3/TooltipBar.js
+++ resources/modules/admin/src/components/dashboard/widgets/d3/TooltipBar.js
@@ -29,32 +29,35 @@ class TooltipBar extends PureComponent {
           className={`${this.props.widgetID} altrp-dashboard__tooltip--font altrp-dashboard__tooltip--label-background altrp-dashboard__tooltip--width altrp-dashboard__tooltip--label-background-shadow altrp-dashboard__tooltip--border-type altrp-dashboard__tooltip--border-width altrp-dashboard__tooltip--border-color  col-12`}
           style={{ padding: "5px 9px" }}
         >
-          <div
-            style={
+          <div>
+            {this.props.datum?.data?.tooltip === undefined &&
+            <div style={
               {
-                // whiteSpace: "pre",
-                // display: "flex",
-                // alignItems: "center"
+                whiteSpace: "pre",
+                display: "flex",
+                alignItems: "center"
               }
+            }>
+              <span
+                style={{
+                  display: "block",
+                  width: "12px",
+                  height: "12px",
+                  background: this.props.datum.color,
+                  marginRight: "7px"
+                }}
+              >  
+              </span>
+              <div className={`${this.props.widgetID} col px-0`}>
+                  {this.props.datum.data.key}:{" "}
+                  <strong
+                    className={`${this.props.widgetID} altrp-dashboard__tooltip--font col px-0`}
+                  >
+                    {this.props.datum.data.value}
+                  </strong>
+                </div>
+              </div>
             }
-          >
-            {/* <span
-              style={{
-                display: "block",
-                width: "12px",
-                height: "12px",
-                background: this.props.datum.color,
-                marginRight: "7px"
-              }}
-            ></span> */}
-            <div className={`${this.props.widgetID} col px-0`}>
-              {this.props.datum.data.key}:{" "}
-              <strong
-                className={`${this.props.widgetID} altrp-dashboard__tooltip--font col px-0`}
-              >
-                {this.props.datum.data.value}
-              </strong>
-            </div>
             {this.props.datum?.data?.tooltip?.map((item, index) => {
               return (
                 <div
@@ -82,12 +85,10 @@ class TooltipBar extends PureComponent {
       <>
         <div
           style={{
-            background: "white",
             color: "inherit",
             fontSize: "{{SIZE}}px",
-            borderRadius: "2px",
-            boxShadow: "rgba(0, 0, 0, 0.25) 0px 1px 2px",
-            padding: "5px 9px"
+            borderRadius: "2px",   
+            padding: "3px 9px"
           }}
         >
           <div
