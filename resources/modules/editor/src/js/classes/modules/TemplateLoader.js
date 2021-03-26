d9139e9f84ff95ca7e12b25a2ff4ee134234ae3d
Dmitry <top506@yandex.ru>
2021-03-26 12:57:49 +0300

    async loading template once

--- resources/modules/editor/src/js/classes/modules/TemplateLoader.js
+++ resources/modules/editor/src/js/classes/modules/TemplateLoader.js
@@ -1,10 +1,23 @@
 import Resource from "../Resource";
 import AltrpModel from "../AltrpModel";
 import frontElementsFabric from "../../../../../front-app/src/js/classes/FrontElementsFabric";
+import {mbParseJSON} from "../../../../../front-app/src/js/helpers";
 /**
  * @class TemplateLoader
  */
 class TemplateLoader {
+  /**
+   * Хранилище статусов получения данных, чтобы не делать много параллельных запросов
+   * @type {{}}
+   */
+  static statuses = {};
+  /**
+   * Хранилище обратных вызовов, чтобы не делать много параллельных запросов
+   * @type {{}}
+   */
+  static pendingCallbacks = {};
+
+
   constructor(){
     this.resource = new Resource({route: '/ajax/templates'});
     this.templatesCache = new AltrpModel({});
@@ -29,6 +42,7 @@ class TemplateLoader {
     }
     return Promise.resolve(this.templatesCache.getProperty(templateId));
   }
+
   /**
    * Загрузить шаблон по ид
    * @param {integer | string} templateId
@@ -36,20 +50,46 @@ class TemplateLoader {
    * @param {*} templateId
    */
   async loadParsedTemplate(templateId, force = false){
-    let update = force;
+
+    if(! templateId){
+      return null;
+    }
+
     templateId = Number(templateId) ? Number(templateId) : templateId;
-    if(! update){
-      update = ! this.templatesCache.hasProperty(templateId);
+
+    if(_.get(TemplateLoader, `statuses.${templateId}`) === 'loading'){
+      return new Promise((resolve, reject) => {
+        TemplateLoader.pendingCallbacks[templateId] = TemplateLoader.pendingCallbacks[templateId] || [];
+        TemplateLoader.pendingCallbacks[templateId].push(resolve);
+      })
     }
-    let template;
-    if(update){
-      template = await this.resource.get(templateId);
-      this.templatesCache.setProperty(templateId, template);
-    } else {
-      template = this.templatesCache.getProperty(templateId);
+
+    TemplateLoader.statuses[templateId] = 'loading';
+
+    try {
+      let templateData = _.get((await this.resource.get(templateId)), 'data') || null;
+      templateData = JSON.parse(templateData);
+
+      if(_.isArray(TemplateLoader.pendingCallbacks[templateId])){
+        TemplateLoader.pendingCallbacks[templateId].forEach(callback=>{
+          callback(frontElementsFabric.parseData(templateData));
+        });
+      }
+
+      TemplateLoader.statuses[templateId] = 'loaded';
+      TemplateLoader.pendingCallbacks[templateId] = [];
+      return frontElementsFabric.parseData(templateData);
+
+    }catch(error){
+      if(_.isArray(TemplateLoader.pendingCallbacks[templateId])){
+        TemplateLoader.pendingCallbacks[templateId].forEach(callback=>{
+          callback(null);
+        });
+      }
+      TemplateLoader.statuses[templateId] = 'loaded';
+      TemplateLoader.pendingCallbacks[templateId] = [];
+      return null;
     }
-    let templateData = JSON.parse(template.data);
-    return frontElementsFabric.parseData(templateData);
   }
 }
 
