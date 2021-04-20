23cc0dc8876b805dc9eb5ce14d44ff5d2741e0e8
Sannikov Alexey <alex617180@mail.ru>
2021-04-20 19:27:09 +0500

    Feature: add dinamic data from env in altrpenv

--- app/Jobs/RunRobotsJob.php
+++ app/Jobs/RunRobotsJob.php
@@ -39,6 +39,7 @@ class RunRobotsJob implements ShouldQueue
         $this->data['altrpuser'] = $currentEnv->current_user ?? null;
         $this->data['altrpmodel'] = $data['record'] ?? null;
         $this->data['altrprequest'] = $currentEnv->request ?? null;
+        $this->data['altrpenv'] = array_change_key_case ($_ENV);
     }
 
     /**
