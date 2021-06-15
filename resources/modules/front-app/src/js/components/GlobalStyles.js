f6b17584b30c6469405d25741f539b44f7da5cfb
shtempel <o_gredi@mail.ru>
2021-06-15 14:58:32 +0300

    chenge GlobalStyles

--- resources/modules/front-app/src/js/components/GlobalStyles.js
+++ resources/modules/front-app/src/js/components/GlobalStyles.js
@@ -5,31 +5,12 @@ import { colorPropertyStyled } from '../helpers/styles';
 
 const GlobalStyles = createGlobalStyle`${({elementsSettings})=>{
   let styles = '';
-  let settingsHeading;
-  let elementId;
 
   _.each(elementsSettings, (item, id) => {
-    if (item.name === 'heading') {
-      settingsHeading = item.settings;
-      elementId = id;
-    }
+
   });
 
-  if (settingsHeading === undefined) {
-    return styles;
-  }
-
-  styles += `.altrp-element${elementId} .altrp-heading, .altrp-element${elementId} .altrp-heading a {`;
-
-  const color = getResponsiveSetting(settingsHeading, 'heading_style_color');
-
-  if (color) {
-    styles += colorPropertyStyled(color, 'color');
-  }
-
-  styles += `} `;
-
-  console.log(styles);
+  styles += `}`;
 
   return styles;
 }}`;
