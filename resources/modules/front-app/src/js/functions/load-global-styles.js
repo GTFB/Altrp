d72af351e4574cea9833e9ad216af384382620a4
shtempel <o_gredi@mail.ru>
2021-06-18 15:56:02 +0300

    global styles loader

---
+++ resources/modules/front-app/src/js/functions/load-global-styles.js
@@ -0,0 +1,44 @@
+import {replaceAllSettings} from "../store/elements-settings/actions";
+
+/**
+ * Загрузка отрисовщика email
+ */
+export  default  function loadGlobalStyles(){
+  import(/* webpackChunkName: 'EmailTemplatesRenderer' */'../components/GlobalStyles').then(module => {
+    const GlobalStyles = module.default;
+    const stylesContainer = document.createElement('div');
+    document.body.appendChild(stylesContainer);
+    ReactDOM.render(<window.Provider store={window.appStore}>
+      <GlobalStyles/>
+    </window.Provider>, stylesContainer)
+  });
+  const settings = {};
+  page_areas.forEach(area=>{
+    const rootElement = _.get(area, 'template.data');
+    if(rootElement){
+      recurseAddSettings(rootElement)
+    }
+    if(_.isArray(_.get(area, 'templates'))){
+      const templates = _.get(area, 'templates');
+      templates.forEach(template=>{
+        recurseAddSettings(template.data)
+      })
+    }
+  });
+
+  /**
+   *
+   * @param {{}} element
+   */
+  function recurseAddSettings(element){
+    if(! element || ! element.id || ! element.settings || ! element.name){
+      return
+    }
+    settings[element.id] = {
+      settings: element.settings,
+      name: element.name,
+    };
+    element.children.forEach(el=>{recurseAddSettings(el)})
+  }
+  appStore.dispatch(replaceAllSettings(settings));
+}
