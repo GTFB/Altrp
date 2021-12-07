6d59f83ee48c7401da981b07be37973859b78055
shtempel <o_gredi@mail.ru>
2021-12-07 14:59:49 +0300

    update menu skeleton

--- server/classes/components/MenuPlaceholder.js
+++ server/classes/components/MenuPlaceholder.js
@@ -4,27 +4,27 @@ import React from "react";
 import SkeletonPlaceholder from "./SkeletonPlaceholder";
 import {mbParseJSON} from "../../../resources/modules/front-app/src/js/helpers";
 
-const Container = styled.div`
+const Container = styled.ul`
   display: flex;
 
  ${props => {
-    let styles = "";
+  let styles = "";
 
-    if(props.type === "vertical") {
-      styles += "flex-direction: column;";
-    } else if(styles === "horizontal") {
-      styles += "flex-direction: row;";
-    }
+  if (props.type === "vertical") {
+    styles += "flex-direction: column;";
+  } else if (styles === "horizontal") {
+    styles += "flex-direction: row;";
+  }
 
-    if(props.gap) {
-      styles += `grid-gap: ${props.gap}`
-    }
+  if (props.gap) {
+    styles += `grid-gap: ${props.gap}`
+  }
 
-    return styles
-  }}
+  return styles
+}}
 `
 
-const MenuPlaceholder = props=>{
+const MenuPlaceholder = props => {
   const divStyles = {
     position: 'relative',
     width: props.element.getResponsiveSetting('skeleton_width') || '100%',
@@ -33,21 +33,27 @@ const MenuPlaceholder = props=>{
 
   const guid = props.element.getResponsiveSetting('menu');
 
-    const menus = window.appStore.getState().altrpMenus;
-  if(guid) {
+  const menus = window.appStore.getState().altrpMenus;
+  if (guid) {
     const menu = menus.find((m) => {
       return m.guid === guid
     });
 
     const items = mbParseJSON(menu.children);
     const type = props.element.getResponsiveSetting("type", "", "vertical");
-    const gap =  props.element.getResponsiveSetting("gap");
+    const gap = props.element.getResponsiveSetting("gap");
 
-    return <Container gap={gap} type={type}>
+
+    return <Container gap={gap} type={type} className="bp3-menu altrp-menu">
       {
-        items.map((i) => (
-          <div style={divStyles} key={i.id}><Skeleton/></div>
-        ))
+        items.map((i) => {
+
+          return (
+            <SkeletonMenuItem className={i?.children?.length ? 'bp3-submenu' : ''}
+                              item={i}
+                              key={i.id}/>
+          )
+        })
       }
     </Container>
   } else {
@@ -55,6 +61,28 @@ const MenuPlaceholder = props=>{
   }
 
 }
+
+const SkeletonMenuItem =
+  ({
+     item
+   }) => {
+    let __html = `
+    ${item.icon ? `<span class="altrp-menu-item__icon">${item.icon}</span>` : ''}
+    <div class="bp3-fill bp3-text-overflow-ellipsis">${item.label}</div>
+    ${item?.children?.length ? `
+      <span class="bp3-icon bp3-icon-caret-right">
+        <svg data-icon="caret-right" width="16" height="16" viewBox="0 0 16 16">
+        <desc>Open sub menu</desc>
+        <path d="M11 8c0-.15-.07-.28-.17-.37l-4-3.5A.495.495 0 006 4.5v7a.495.495 0 00.83.37l4-3.5c.1-.09.17-.22.17-.37z" fill-rule="evenodd">
+        </path>
+        </svg>
+      </span>` : ''}
+    `;
+    let link = <a href={item.url} dangerouslySetInnerHTML={{__html: __html}}/>
+    return (
+      <li className={item?.children?.length ? 'bp3-submenu' : ''} key={item.id}>{link}</li>
+    )
+  }
 
 export default MenuPlaceholder;
 
