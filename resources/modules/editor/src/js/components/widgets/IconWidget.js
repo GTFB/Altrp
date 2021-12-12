795d9f8521bff90c10b4f0cc0ff300c881bacdfd
Serejik <sergey.sirsilin@gmail.com>
2021-12-12 21:05:35 +0400

    add default title description to icon

--- resources/modules/editor/src/js/components/widgets/IconWidget.js
+++ resources/modules/editor/src/js/components/widgets/IconWidget.js
@@ -1,11 +1,5 @@
 const {
-  getComponentByElementId,
-  getHTMLElementById,
-  isEditor,
-  parseURLTemplate,
-  printElements,
   renderAsset,
-  scrollToElement
 } = window.altrpHelpers;
 
 class IconWidget extends Component {
@@ -23,9 +17,7 @@ class IconWidget extends Component {
       this.render = props.baseRender(this);
     }
   }
-  /**
-   * Компонент удаляется со страницы
-   */
+
   async _componentWillUnmount() {
     const actionsManager = (
       await import(/* webpackChunkName: 'ActionsManager' */
@@ -36,12 +28,14 @@ class IconWidget extends Component {
   }
 
   render() {
+    console.log({title: this.state.settings.title_text});
+
     return (
       <div className='icon-widget-wrapper'>
         <span className="widget-icon">{renderAsset(this.state.settings.icon)}</span>
         <div className="content">
-          <h3 className="title">{this.state.settings.title_text}</h3>
-          <div className="description">{this.state.settings.description}</div>
+          <h3 className="title">{this.state.settings.title_text === undefined && 'Title'}</h3>
+          <div className="description">{this.state.settings.description === undefined && 'Icon description'}</div>
         </div>
       </div>
     )
