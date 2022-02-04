3a50a03f192c2c6cae2dda1c31da926ccc28cf29
altrp-dsavelyev <altds@academ-x.ur>
2022-02-04 12:40:43 +0300

    ts-ignore

--- altrpnjs/database/migrations/1643966145506_update_template_guids.ts
+++ altrpnjs/database/migrations/1643966145506_update_template_guids.ts
@@ -22,6 +22,7 @@ export default class UpdateTemplateGuids extends BaseSchema {
 
   public async down () {
     this.schema.alterTable(this.tableName, (table) => {
+      //@ts-ignore
       table.dropUnique("guid")
     })
   }
