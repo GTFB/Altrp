51dc01bef9c7c57b1a8d15b715a61c700c7321a4
daniil savelyev <44727718+dsavelyev-man@users.noreply.github.com>
2022-02-08 00:29:41 +0300

    check email

--- resources/modules/editor/src/js/decorators/actions-controllers.js
+++ resources/modules/editor/src/js/decorators/actions-controllers.js
@@ -222,6 +222,49 @@ export function actionsControllers(
     }
   });
 
+  actionsRepeater.addControl('socket_type', {
+    type: CONTROLLER_SELECT,
+    label: 'Type',
+    options: [
+      {
+        value: "user",
+        label: "User"
+      },
+      {
+        value: "custom",
+        label: "Custom"
+      },
+    ],
+    conditions: {
+      type: ["socket_receiver"]
+    }
+  });
+
+  actionsRepeater.addControl('socket_emit_name', {
+    type: CONTROLLER_TEXTAREA,
+    label: 'Name',
+    conditions: {
+      type: ["socket_emit"],
+    }
+  });
+
+  actionsRepeater.addControl('socket_value', {
+    type: CONTROLLER_TEXTAREA,
+    label: 'Value',
+    conditions: {
+      type: ["socket_emit"],
+    }
+  });
+
+  actionsRepeater.addControl('socket_name', {
+    type: CONTROLLER_TEXTAREA,
+    label: 'Name',
+    conditions: {
+      type: ["socket_receiver"],
+      socket_type: "custom"
+    }
+  });
+
   actionsRepeater.addControl('aliases', {
     type: CONTROLLER_TEXTAREA,
     dynamic: false,
