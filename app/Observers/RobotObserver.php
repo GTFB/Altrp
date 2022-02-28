b57685089064f6b3b48365134c47169d11a48ab7
Dmitry <top506@yandex.ru>
2022-02-28 09:37:22 +0300

    Telegram bot bug fix

--- app/Observers/RobotObserver.php
+++ app/Observers/RobotObserver.php
@@ -86,7 +86,13 @@ class RobotObserver
                 $base_url = substr(env('APP_URL'), -1) == "/" ? substr(env('APP_URL'),0,-1) : env('APP_URL');
 
                 try {
-                  $req = $client->get("https://api.telegram.org/bot".$robot->getTelegramBotToken()."/setWebhook?url=".$base_url."/api/altrp_models/".$model->name."/robots/".$robot->name);
+                  $model_source = Source::where('model_id', $model->id)
+                    ->where('type', 'add')
+                    ->where('request_type', 'post')
+                    ->first();
+                  $url = "https://api.telegram.org/bot".$robot->getTelegramBotToken()."/setWebhook?url=".$base_url."/api/altrp_models".$model_source->api_url."/robots/".$robot->name;
+                  $req = $client->get($url);
+
                   $response = json_decode($req->getBody()->getContents(),true);
                   if ($response['result']) {
                     $robot->is_active = 1;
@@ -185,12 +191,20 @@ class RobotObserver
                 throw new RouteGenerateFailedException( 'Failed to generate routes', 500 );
               }
 
+              $base_url = substr(env('APP_URL'), -1) == "/" ? substr(env('APP_URL'),0,-1) : env('APP_URL');
+
               if (!$robot->is_active && $_SERVER['REQUEST_SCHEME'] == "https") {
                 $client = new \GuzzleHttp\Client();              
                 $base_url = substr(env('APP_URL'), -1) == "/" ? substr(env('APP_URL'),0,-1) : env('APP_URL');
 
                 try {
-                  $req = $client->get("https://api.telegram.org/bot".$robot->getTelegramBotToken()."/setWebhook?url=".$base_url."/api/altrp_models/".$model->name."/robots/".$robot->name);
+                  $model_source = Source::where('model_id', $model->id)
+                    ->where('type', 'add')
+                    ->where('request_type', 'post')
+                    ->first();
+                  $url = "https://api.telegram.org/bot".$robot->getTelegramBotToken()."/setWebhook?url=".$base_url."/api/altrp_models".$model_source->api_url."/robots/".$robot->name;
+
+                  $req = $client->get($url);
                   $response = json_decode($req->getBody()->getContents(),true);
                   if ($response['result']) {
                     $robot->is_active = 1;
