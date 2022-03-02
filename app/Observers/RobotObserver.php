2c3dde5c16dbfa69c47df01e437ed9c940b765be
Dmitry <top506@yandex.ru>
2022-03-02 17:59:24 +0300

    updated Robot Observer

--- app/Observers/RobotObserver.php
+++ app/Observers/RobotObserver.php
@@ -14,6 +14,7 @@ use App\Altrp\Source;
 use App\Exceptions\RouteGenerateFailedException;
 use App\PageDatasource;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class RobotObserver
 {
@@ -85,17 +86,20 @@ class RobotObserver
               DB::table('altrp_robots')
                         ->where('start_condition', 'telegram_bot')
                         ->where('id', '!=', $robot->id)
-                        ->update(['is_active' => 0]);
+                        ->update(['is_active' => 0]);         
 
               if ($robot->start_condition == "telegram_bot" && $_SERVER['REQUEST_SCHEME'] == "https") {
                 $client = new \GuzzleHttp\Client();              
-                $base_url = substr(env('APP_URL'), -1) == "/" ? substr(env('APP_URL'),0,-1) : env('APP_URL');
+                
                 try {
                   $model_source = Source::where('model_id', $model->id)
                     ->where('type', 'add')
                     ->where('request_type', 'post')
                     ->first();
+
+                  $base_url = substr(env('APP_URL'), -1) == "/" ? substr(env('APP_URL'),0,-1) : env('APP_URL');
                   $url = "https://api.telegram.org/bot".$robot->getTelegramBotToken()."/setWebhook?url=".$base_url."/api/altrp_models".$model_source->api_url."/robots/".$robot->name;
+
                   $req = $client->get($url);
                   $response = json_decode($req->getBody()->getContents(),true);
 
@@ -199,17 +203,20 @@ class RobotObserver
               DB::table('altrp_robots')
                         ->where('start_condition', 'telegram_bot')
                         ->where('id', '!=', $robot->id)
-                        ->update(['is_active' => 0]);
+                        ->update(['is_active' => 0]);        
 
-              if ($robot->start_condition == "telegram_bot" && !$robot->is_active && $_SERVER['REQUEST_SCHEME'] == "https") {
+              if ($robot->start_condition == "telegram_bot" && $_SERVER['REQUEST_SCHEME'] == "https") {
                 $client = new \GuzzleHttp\Client();              
-                $base_url = substr(env('APP_URL'), -1) == "/" ? substr(env('APP_URL'),0,-1) : env('APP_URL');
+                
                 try {
                   $model_source = Source::where('model_id', $model->id)
                     ->where('type', 'add')
                     ->where('request_type', 'post')
                     ->first();
+
+                  $base_url = substr(env('APP_URL'), -1) == "/" ? substr(env('APP_URL'),0,-1) : env('APP_URL');
                   $url = "https://api.telegram.org/bot".$robot->getTelegramBotToken()."/setWebhook?url=".$base_url."/api/altrp_models".$model_source->api_url."/robots/".$robot->name;
+
                   $req = $client->get($url);
                   $response = json_decode($req->getBody()->getContents(),true);
 
