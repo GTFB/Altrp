4f76da5d9a72af461b7a5dc36dc4592f25452cf9
dsavelyev <altds@academ-x.ru>
2022-03-15 17:49:53 +0300

    datasource controller

---
+++ altrpnjs/app/Models/GlobalStyle.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import {BaseModel, column} from '@ioc:Adonis/Lucid/Orm'
+
+export default class GlobalStyle extends BaseModel {
+  public static table = "altrp_global_template_styles";
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column({
+    serialize: (value: string) => {
+      return JSON.parse(value)
+    }
+  })
+  public settings: string
+
+  @column()
+  public type: string
+
+  @column()
+  public guid: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
