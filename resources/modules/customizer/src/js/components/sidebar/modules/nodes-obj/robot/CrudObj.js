c378fe163c60d9464b7cce38238fbfbc6b51c1e0
dsavelyev <altds@academ-x.ru>
2022-03-17 07:15:03 +0300

    robots and customizer merged

---
+++ resources/modules/customizer/src/js/components/sidebar/modules/nodes-obj/robot/CrudObj.js
@@ -0,0 +1,12 @@
+import CrudIcon from "../../../../../../svgs/crud.svg";
+import Crud from "../../robot/data/action/Crud";
+import CrudAction from "../../robot/widgets/CrudAction";
+
+export const CrudObj = {
+  title: 'Crud',
+  name: 'crudAction',
+  selectedNode: Crud,
+  node: CrudAction,
+  icon: CrudIcon,
+  type: "robot",
+}
