f1cafc617d49304277c3eaac9053dd36d455f46a
shtempel <525@talp.ru>
2022-05-18 12:56:13 +0300

    delete data response when template updates

--- altrpnjs/app/Models/Model.ts
+++ altrpnjs/app/Models/Model.ts
@@ -100,7 +100,6 @@ export default class Model extends BaseModel {
   public user_id: number
 
   @belongsTo(() => User, {
-    //foreignKey: "author"
     foreignKey: "user_id"
   })
   public user: BelongsTo<typeof User>
