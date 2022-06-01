cd5b929bb058222fe2db514b2643a4b5cc86da9f
shtempel <525@talp.ru>
2022-06-01 21:19:43 +0300

    updates with progressbars

--- resources/modules/admin/src/js/functions/progressBar.js
+++ resources/modules/admin/src/js/functions/progressBar.js
@@ -1,4 +1,4 @@
-
+import '../../sass/components/altrp-progress-bar.scss'
 import {  ProgressBar,Intent } from "@blueprintjs/core";
 export default function progressBar(value){
   let progressBar = document.getElementById('altrp-progress-bar')
@@ -8,7 +8,6 @@ export default function progressBar(value){
     document.body.appendChild(progressBar)
   }
   if (value){
-    console.log(value);
     ReactDOM.render(<ProgressBar value={value} intent={Intent.PRIMARY}/>, progressBar)
   } else {
     document.body.removeChild(progressBar)
--- resources/modules/admin/src/js/functions/upgradeBackend.js
+++ resources/modules/admin/src/js/functions/upgradeBackend.js
@@ -2,29 +2,68 @@ import axios from "axios"
 import delay from "../../../../front-app/src/js/functions/delay"
 import progressBar from "./progressBar";
 
-async function upgradeBackend() {
-  const types = [
-    'listeners',
-    'models',
-    'pages',
-    'templates',
-  ]
-  const pages =  (await axios.get('/admin/ajax/pages')).data
-  const count = 4 + pages?.length
-  let i = 1
-  for (let type of types) {
-    let ids = ['']
-    if (type === 'pages') {
-      ids = pages.map(i =>i.id)
+const defaultTypes = [
+  'listeners',
+  'models',
+  'pages',
+  'templates',
+]
+
+async function upgradeBackend(types = defaultTypes, ids ) {
+
+  progressBar(.001)
+  let pages = ['']
+  if(types.indexOf('pages') > -1){
+    pages = (await axios.get('/admin/ajax/pages')).data
+  }
+  let templates = ['']
+  if(types.indexOf('templates') > -1) {
+    try {
+      templates = (await axios.get('/admin/ajax/templates/getAllIds')).data?.data
+    } catch (e) {
+      templates = ['']
     }
-    for (let id of ids) {
+  }
+  const delta = 3
+  let count = types.filter(t=> ['pages', 'templates'].indexOf(t) === -1).length
+  if(ids){
+    count += ids.length / 3
+  } else {
+    count += pages?.length / 3 + templates?.length / 3
+  }
+
+  let _i = 1
+  for (let type of types) {
+    if(! ids){
+      switch (type) {
+        case 'pages': {
+          ids = pages.map(i => i.id)
+
+        }
+          break;
+        case 'templates': {
+          ids = templates.map(i => i.id)
+        }
+          break;
+      }
+    }
+
+    if(!_.isArray(ids)){
+      ids = ['']
+    }
+    do{
+      let _ids = ids.splice(0,delta)
+      _ids = _ids.join(',')
+
       try {
-        await axios.post('/admin/ajax/update-all-resources', {type, id})
-        let progress = i ++ / count
+        await axios.post('/admin/ajax/update-all-resources', {type, id: _ids})
+        let progress = _i++ / count
         progressBar(progress)
 
       } catch (e) {
         console.error(e);
+        let progress = _i++ / count
+        progressBar(progress)
         let serverRestarted = false
         let i = 0
         do {
@@ -38,7 +77,8 @@ async function upgradeBackend() {
           }
         } while (!serverRestarted && i < 10)
       }
-    }
+    }while (ids.length)
+    ids = null
   }
 }
 
