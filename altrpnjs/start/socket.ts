9940c9cbea219722a91a6af569f18bae930bd603
shtempel <525@talp.ru>
2022-06-08 22:38:45 +0300

    new front app loader

--- altrpnjs/start/socket.ts
+++ altrpnjs/start/socket.ts
@@ -8,10 +8,14 @@ Ws.io.on("connection", async (socket) => {
     const guid = await Ws.pushClient(socket)
 
     socket.on("disconnect", () => {
+      console.log('disconnect');
       Ws.removeClient(guid, socket)
     })
 
-    // socket.on("message", (message) => {
-    // })
+    socket.on("message", (message) => {
+      if(message === 'altrp-front-load'){
+        socket.send('altrpe')
+      }
+    })
   }
 })
