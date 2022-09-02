69b0c20bb0c32001d03b5e80ec17f2f66738b29d
shtempel <525@talp.ru>
2022-09-02 17:14:33 +0300

    * Fix Image Widget Responsive Size Issue

--- altrpnjs/config/app.ts
+++ altrpnjs/config/app.ts
@@ -234,5 +234,5 @@ export const profiler: ProfilerConfig = {
 export const validator: ValidatorConfig = {
 }
 
-export const altrp_version:string = '1.2.6'
+export const altrp_version:string = '1.2.7'
 export const url:string  = Env.get('APP_URL')
