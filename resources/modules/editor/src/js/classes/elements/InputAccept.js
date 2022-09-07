5ece9af2928b2bc52889e58ec1cf8eb2fb0ac377
hryschiller <alths@academ-x.ru>
2022-09-07 11:50:59 -0400

    optimize styles of input-accept

--- resources/modules/editor/src/js/classes/elements/InputAccept.js
+++ resources/modules/editor/src/js/classes/elements/InputAccept.js
@@ -129,7 +129,6 @@ class InputAccept extends BaseElement {
     this.addControl("label_icon_position", {
       type: CONTROLLER_SELECT,
       label: "Icon Position",
-      default: "default",
       options: [
         {
           value: "row",
@@ -369,10 +368,6 @@ class InputAccept extends BaseElement {
     this.addControl("label_position_top", {
       type: CONTROLLER_SLIDER,
       label: "Label Y Position",
-      default: {
-        unit: "px",
-        size: null
-      },
       conditions: {
         content_label_position_type: ["absolute"]
       },
@@ -388,10 +383,6 @@ class InputAccept extends BaseElement {
     this.addControl("label_position_left", {
       type: CONTROLLER_SLIDER,
       label: "Label X Position",
-      default: {
-        unit: "px",
-        size: null
-      },
       conditions: {
         content_label_position_type: ["absolute"]
       },
@@ -407,10 +398,6 @@ class InputAccept extends BaseElement {
     this.addControl("label_width", {
       type: CONTROLLER_SLIDER,
       label: "Label Width",
-      default: {
-        unit: "%",
-        size: null
-      },
       units: ['px', '%', 'vh', 'vw'],
       max: 100,
       min: 0
@@ -744,8 +731,7 @@ class InputAccept extends BaseElement {
           label: "Bottom",
           value: "column-reverse"
         }
-      ],
-      default: "left"
+      ]
     });
 
     this.endControlSection();
