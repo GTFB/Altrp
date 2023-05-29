83bac55a2b4071cbba2e00f8524b8427ceaa0544
Dmitry <529@talp.ru>
2023-05-29 22:48:44 +0300

    getting media path variations

--- altrpnjs/app/Models/Media.ts
+++ altrpnjs/app/Models/Media.ts
@@ -7,6 +7,10 @@ import {
 } from '@ioc:Adonis/Lucid/Orm'
 import User from 'App/Models/User';
 import Category from "App/Models/Category";
+import fs from "fs";
+import base_path from "../../helpers/path/base_path";
+import sizeOf from 'image-size';
+import sharp from 'sharp';
 
 export default class Media extends BaseModel {
 
@@ -85,5 +89,81 @@ export default class Media extends BaseModel {
     pivotRelatedForeignKey: 'category_guid',
   })
   public categories: ManyToMany<typeof Category>
+
+  public getThumbnail(): string {
+    return this.getPathVariation(150, 150)
+  }
+
+  public getCard(): string {
+    return this.getPathVariation(300, 300)
+  }
+
+  public getGood(): string {
+    return this.getPathVariation(600, 600)
+  }
+
+  public getStandart(): string {
+    return this.getPathVariation(1600, 1080)
+  }
+
+  private getPathVariation(width, height): string {
+    
+    const url = this.url
+    const parts = url.split('/');
+    const originalFileName = parts[5];
+    const ext = originalFileName.split('.').pop();
+    const folder = base_path(`/public/${url.replace(originalFileName, '')}`)
+    const requestedFileName = originalFileName.split('.')[0]+'_'+width+'x'+height+'.'+ext;
+
+    var files: any[] = []
+    files = fs.readdirSync(folder).filter(fn => fn.startsWith(requestedFileName));
+
+    const requestedPath = url.replace(originalFileName, requestedFileName);
+
+    if (files.length > 0) {
+      return requestedPath
+    }
+    else {
+      files = fs.readdirSync(folder).filter(fn => fn.startsWith(originalFileName.split('.')[0]));
+    }
+
+    if (files.length > 0) {
+      const originalPath = folder+originalFileName
+
+      if (fs.existsSync(originalPath)){
+        
+        const searchFilename = folder+requestedFileName
+        const dimensions: any = sizeOf(originalPath)
+        var targetAspectRatio = height / width;
+        var sourceAspectRatio = dimensions.height / dimensions.width;
+
+        if (targetAspectRatio == 1) {
+          if (dimensions.width > dimensions.height) {
+            height = Math.round(height*(dimensions.height/dimensions.width));
+          }
+          if (dimensions.width < dimensions.height) {
+            width = Math.round(width*(dimensions.width/dimensions.height));
+          }
+        } else if(targetAspectRatio < 1 && targetAspectRatio > sourceAspectRatio) {
+          height = Math.round(width * sourceAspectRatio);
+        } else if(targetAspectRatio < 1 && targetAspectRatio < sourceAspectRatio){
+          width = Math.round(height * sourceAspectRatio);
+        }
+
+        if(ext == 'webp'){
+          if (width > 0 && height > 0) {
+            sharp(originalPath).toFormat('webp').resize(width, height).toFile(searchFilename);
+          } else {
+            sharp(originalPath).toFormat('webp').toFile(searchFilename);
+          }
+          return requestedPath
+        } else if(width > 0 && height > 0) {
+          sharp(originalPath).resize(width, height).toFile(searchFilename);
+          return requestedPath
+        }
+      }
+    }
+    return '';
+  }
 
 }
